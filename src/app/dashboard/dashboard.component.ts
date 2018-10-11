fc472598d7737e2087b01a326923b0bbbe014caa
amirch1 <amir.chervinsky@kaltura.com>
2018-10-11 16:09:01 +0300

    tooltip fix for date picker

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { KalturaLogger } from '@kaltura-ng/kaltura-logger';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
