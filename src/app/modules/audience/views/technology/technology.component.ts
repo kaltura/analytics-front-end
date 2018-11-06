31f9574de2a4b770393ab7a4cc275d0f8f4ba305
Stas <staskgt@gmail.com>
2018-11-06 16:53:18 +0200

    wip

--- src/app/modules/audience/views/technology/technology.component.ts
+++ src/app/modules/audience/views/technology/technology.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DateChangeEvent, DateRanges } from 'shared/components/date-filter/date-filter.service';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
 
 @Component({
   selector: 'app-technology',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technology.component.scss']
 })
 export class TechnologyComponent implements OnInit {
-
-  constructor() { }
-
+  public _dateRange = DateRanges.Last30D;
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _isBusy = false;
+  
+  constructor() {
+  }
+  
   ngOnInit() {
   }
-
+  
+  public _onDateFilterChange(event: DateChangeEvent): void {
+  
+  }
+  
 }
