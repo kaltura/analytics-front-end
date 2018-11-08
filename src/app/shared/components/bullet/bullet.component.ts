e16dfb8c5e602bab02378038a8fbb0214879946d
Stas <staskgt@gmail.com>
2018-11-08 09:17:23 +0200

    fix typo

--- src/app/shared/components/bullet/bullet.component.ts
+++ src/app/shared/components/bullet/bullet.component.ts
@@ -9,6 +9,6 @@ export class BulletComponent {
   public _value = 0;
   
   @Input() set value(value: any) {
-    this._value = Number(parseFloat(value).toFixed(2)));
+    this._value = Number(parseFloat(value).toFixed(2));
   }
 }
