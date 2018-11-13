6d8e131b6f757eb11e1f96a278faa772b017d093
Stas <staskgt@gmail.com>
2018-11-13 13:28:09 +0200

    add highlights report

--- src/app/modules/audience/views/engagement/highlights/highlights.component.ts
+++ src/app/modules/audience/views/engagement/highlights/highlights.component.ts
@@ -1,17 +1,254 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EngagementBaseReportComponent } from '../engagement-base-report/engagement-base-report.component';
+import { Tab } from 'shared/components/report-tabs/report-tabs.component';
+import { KalturaFilterPager, KalturaReportGraph, KalturaReportInputFilter, KalturaReportInterval, KalturaReportTable, KalturaReportTotal, KalturaReportType } from 'kaltura-ngx-client';
+import { analyticsConfig } from 'configuration/analytics-config';
+import { AreaBlockerMessage, AreaBlockerMessageButton } from '@kaltura-ng/kaltura-ui';
+import { AuthService, ErrorDetails, ErrorsManagerService, Report, ReportConfig, ReportService } from 'shared/services';
+import { map, switchMap } from 'rxjs/operators';
+import { of as ObservableOf } from 'rxjs';
+import { CompareService } from 'shared/services/compare.service';
+import { ReportDataConfig } from 'shared/services/storage-data-base.config';
+import { TranslateService } from '@ngx-translate/core';
+import { HighlightsConfig } from './highlights.config';
+import { DateFilterComponent } from 'shared/components/date-filter/date-filter.component';
 
 @Component({
   selector: 'app-engagement-highlights',
   templateUrl: './highlights.component.html',
-  styleUrls: ['./highlights.component.scss']
+  styleUrls: ['./highlights.component.scss'],
+  providers: [HighlightsConfig, ReportService]
 })
 export class EngagementHighlightsComponent extends EngagementBaseReportComponent implements OnInit {
+  @Input() dateFilterComponent: DateFilterComponent;
+  
+  private _order = '-count_plays';
+  private _reportType = KalturaReportType.userEngagement;
+  private _dataConfig: ReportDataConfig;
+  
+  public _columns: string[] = [];
+  public _isBusy: boolean;
+  public _chartDataLoaded = false;
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _tabsData: Tab[] = [];
+  public _tableData: any[] = [];
+  public _selectedMetrics: string;
+  public _reportInterval: KalturaReportInterval = KalturaReportInterval.months;
+  public _chartType = 'line';
+  public _compareFilter: KalturaReportInputFilter = null;
+  public _lineChartData = {};
+  public _showTable = false;
+  public _totalCount = 0;
+  public _pager: KalturaFilterPager = new KalturaFilterPager({ pageSize: 25, pageIndex: 1 });
+  public _filter: KalturaReportInputFilter = new KalturaReportInputFilter(
+    {
+      searchInTags: true,
+      searchInAdminTags: false
+    }
+  );
+  
+  public get _isCompareMode(): boolean {
+    return this._compareFilter !== null;
+  }
+  
+  constructor(private _translate: TranslateService,
+              private _reportService: ReportService,
+              private _compareService: CompareService,
+              private _errorsManager: ErrorsManagerService,
+              private _authService: AuthService,
+              private _dataConfigService: HighlightsConfig) {
+    super();
+    
+    this._dataConfig = _dataConfigService.getConfig();
+    this._selectedMetrics = this._dataConfig.totals.preSelected;
+  }
+  
+  
   ngOnInit() {
   }
   
-  protected _loadReport(): void {
-    console.log('EngagementHighlightsComponent - loadReport');
+  protected _loadReport(sections = this._dataConfig): void {
+    this._updateFilter();
+    
+    this._tableData = [];
+    this._isBusy = true;
+    this._blockerMessage = null;
+    
+    const reportConfig: ReportConfig = { reportType: this._reportType, filter: this._filter, pager: this._pager, order: this._order };
+    this._reportService.getReport(reportConfig, sections)
+      .pipe(switchMap(report => {
+        if (!this._isCompareMode) {
+          return ObservableOf({ report, compare: null });
+        }
+        
+        const compareReportConfig = { reportType: this._reportType, filter: this._compareFilter, pager: this._pager, order: this._order };
+        return this._reportService.getReport(compareReportConfig, sections)
+          .pipe(map(compare => ({ report, compare })));
+      }))
+      .subscribe(({ report, compare }) => {
+          if (compare) {
+            this.handleCompare(report, compare);
+          } else {
+            if (report.table && report.table.header && report.table.data) {
+              this.handleTable(report.table); // handle table
+            }
+            if (report.graphs.length) {
+              this._chartDataLoaded = false;
+              this._handleGraphs(report.graphs); // handle graphs
+            }
+            if (report.totals) {
+              this._handleTotals(report.totals); // handle totals
+            }
+          }
+          this._isBusy = false;
+        },
+        error => {
+          this._isBusy = false;
+          const err: ErrorDetails = this._errorsManager.getError(error);
+          let buttons: AreaBlockerMessageButton[] = [];
+          if (err.forceLogout) {
+            buttons = [{
+              label: this._translate.instant('app.common.ok'),
+              action: () => {
+                this._blockerMessage = null;
+                this._authService.logout();
+              }
+            }];
+          } else {
+            buttons = [{
+              label: this._translate.instant('app.common.close'),
+              action: () => {
+                this._blockerMessage = null;
+              }
+            },
+              {
+                label: this._translate.instant('app.common.retry'),
+                action: () => {
+                  this._loadReport();
+                }
+              }];
+          }
+          this._blockerMessage = new AreaBlockerMessage({
+            title: err.title,
+            message: err.message,
+            buttons
+          });
+        });
   }
-
+  
+  private _updateFilter(): void {
+    this._chartDataLoaded = false;
+    this._filter.timeZoneOffset = this._dateFilter.timeZoneOffset;
+    this._filter.fromDay = this._dateFilter.startDay;
+    this._filter.toDay = this._dateFilter.endDay;
+    this._filter.interval = this._dateFilter.timeUnits;
+    this._reportInterval = this._dateFilter.timeUnits;
+    this._pager.pageIndex = 1;
+    if (this._dateFilter.compare.active) {
+      const compare = this._dateFilter.compare;
+      this._compareFilter = new KalturaReportInputFilter(
+        {
+          searchInTags: true,
+          searchInAdminTags: false,
+          timeZoneOffset: this._dateFilter.timeZoneOffset,
+          interval: this._dateFilter.timeUnits,
+          fromDay: compare.startDay,
+          toDay: compare.endDay,
+        }
+      );
+    } else {
+      this._compareFilter = null;
+    }
+  }
+  
+  private handleCompare(current: Report, compare: Report): void {
+    const currentPeriod = { from: this._filter.fromDay, to: this._filter.toDay };
+    const comparePeriod = { from: this._compareFilter.fromDay, to: this._compareFilter.toDay };
+    
+    if (current.table && compare.table) {
+      const { columns, tableData } = this._compareService.compareTableData(
+        currentPeriod,
+        comparePeriod,
+        current.table,
+        compare.table,
+        this._dataConfig.table
+      );
+      this._totalCount = current.table.totalCount;
+      this._columns = columns;
+      this._tableData = tableData;
+    }
+    
+    if (current.totals && compare.totals) {
+      this._tabsData = this._compareService.compareTotalsData(
+        currentPeriod,
+        comparePeriod,
+        current.totals,
+        compare.totals,
+        this._dataConfig.totals,
+        this._selectedMetrics
+      );
+    }
+    
+    if (current.graphs.length && compare.graphs.length) {
+      const { lineChartData } = this._compareService.compareGraphData(
+        currentPeriod,
+        comparePeriod,
+        current.graphs,
+        compare.graphs,
+        this._dataConfig.graph,
+        this._reportInterval,
+        () => this._chartDataLoaded = true
+      );
+      this._lineChartData = lineChartData;
+    }
+  }
+  
+  private handleTable(table: KalturaReportTable): void {
+    const { columns, tableData } = this._reportService.parseTableData(table, this._dataConfig.table);
+    this._totalCount = table.totalCount;
+    this._columns = columns;
+    this._tableData = tableData;
+  }
+  
+  private _handleTotals(totals: KalturaReportTotal): void {
+    this._tabsData = this._reportService.parseTotals(totals, this._dataConfig.totals, this._selectedMetrics);
+  }
+  
+  private _handleGraphs(graphs: KalturaReportGraph[]): void {
+    const { lineChartData } = this._reportService.parseGraphs(
+      graphs,
+      this._dataConfig.graph,
+      this._reportInterval,
+      () => this._chartDataLoaded = true
+    );
+    this._lineChartData = lineChartData;
+  }
+  
+  public _onTabChange(tab: Tab): void {
+    this._selectedMetrics = tab.key;
+  }
+  
+  public _toggleTable(): void {
+    this._showTable = !this._showTable;
+    if (analyticsConfig.callbacks && analyticsConfig.callbacks.updateLayout) {
+      analyticsConfig.callbacks.updateLayout();
+    }
+  }
+  
+  public _onPaginationChanged(event: any): void {
+    if (event.page !== (this._pager.pageIndex - 1)) {
+      this._pager.pageIndex = event.page + 1;
+      this._loadReport({ table: null });
+    }
+  }
+  
+  public _onSortChanged(event) {
+    if (event.data.length && event.field && event.order && !this._isCompareMode) {
+      const order = event.order === 1 ? '+' + event.field : '-' + event.field;
+      if (order !== this._order) {
+        this._order = order;
+        this._loadReport({ table: null });
+      }
+    }
+  }
 }
---
+++ src/app/modules/audience/views/engagement/highlights/highlights.config.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
+import { ReportHelper } from 'shared/services';
+import { fileSize } from 'shared/utils/file-size';
+
+@Injectable()
+export class HighlightsConfig extends ReportDataBaseConfig {
+  constructor(_translate: TranslateService) {
+    super(_translate);
+  }
+  
+  public getConfig(): ReportDataConfig {
+    return {
+      [ReportDataSection.graph]: {
+        fields: {
+          'count_plays': {
+            format: value => value,
+          },
+          'sum_time_viewed': {
+            format: value => value,
+          }
+        }
+      },
+      [ReportDataSection.table]: {
+        fields: {
+          'count_plays': {
+            format: value => value,
+          },
+          'sum_time_viewed': {
+            format: value => value,
+          }
+        }
+      },
+      [ReportDataSection.totals]: {
+        preSelected: 'count_plays',
+        fields: {
+          'count_plays': {
+            format: value => ReportHelper.numberOrZero(value),
+            title: this._translate.instant(`app.engagement.highlightsReport.count_plays`),
+            tooltip: this._translate.instant(`app.engagement.highlightsReport.count_plays_tt`),
+          },
+          'sum_time_viewed': {
+            format: value => ReportHelper.numberOrZero(value),
+            title: this._translate.instant(`app.engagement.highlightsReport.sum_time_viewed`),
+            tooltip: this._translate.instant(`app.engagement.highlightsReport.sum_time_viewed_tt`),
+            units: () => 'min'
+          }
+        }
+      }
+    };
+  }
+}
