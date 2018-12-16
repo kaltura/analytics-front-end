070719be37883f52b36786338d9ebf55aad237c9
amir.chervinsky <amir.chervinsky@kaltura.com>
2018-12-16 11:37:16 +0200

    change clientTag to kmc-analytics + version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -99,7 +99,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this._logger.info(`Setting ks in ngx-client: ${analyticsConfig.ks}`);
     this._kalturaServerClient.setOptions({
       endpointUrl: getKalturaServerUri(),
-      clientTag: 'analytics'
+      clientTag: 'kmc-analytics:v0.1'
     });
     this._kalturaServerClient.setDefaultRequestOptions({
       ks: analyticsConfig.ks
