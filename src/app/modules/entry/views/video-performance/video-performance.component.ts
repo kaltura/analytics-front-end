710295544e9144c681a65b7f10a3dbb3433426ea
Stas <staskgt@gmail.com>
2019-01-16 16:21:10 +0200

    wip

---
+++ src/app/modules/entry/views/video-performance/video-performance.component.ts
@@ -0,0 +1,166 @@
+import { Component, Input } from '@angular/core';
+import { Tab } from 'shared/components/report-tabs/report-tabs.component';
+import { KalturaFilterPager, KalturaObjectBaseFactory, KalturaReportInputFilter, KalturaReportInterval, KalturaReportTotal, KalturaReportType } from 'kaltura-ngx-client';
+import { AreaBlockerMessage, AreaBlockerMessageButton } from '@kaltura-ng/kaltura-ui';
+import { AuthService, ErrorDetails, ErrorsManagerService, Report, ReportConfig, ReportService } from 'shared/services';
+import { map, switchMap } from 'rxjs/operators';
+import { of as ObservableOf } from 'rxjs';
+import { CompareService } from 'shared/services/compare.service';
+import { ReportDataConfig } from 'shared/services/storage-data-base.config';
+import { TranslateService } from '@ngx-translate/core';
+import { VideoPerformanceConfig } from './video-performance.config';
+import { FrameEventManagerService } from 'shared/modules/frame-event-manager/frame-event-manager.service';
+import { DateChangeEvent } from 'shared/components/date-filter/date-filter.service';
+import { EntryBase } from '../entry-base/entry-base';
+
+@Component({
+  selector: 'app-video-performance',
+  templateUrl: './video-performance.component.html',
+  styleUrls: ['./video-performance.component.scss'],
+  providers: [VideoPerformanceConfig, ReportService]
+})
+export class VideoPerformanceComponent extends EntryBase {
+  @Input() entryId = '';
+
+  private _order = '-month_id';
+  private _reportType = KalturaReportType.userTopContent;
+  private _dataConfig: ReportDataConfig;
+  
+  protected _dateFilter: DateChangeEvent;
+  protected _componentId = 'totals';
+
+  public _isBusy: boolean;
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _tabsData: Tab[] = [];
+  public _reportInterval = KalturaReportInterval.days;
+  public _compareFilter: KalturaReportInputFilter = null;
+  public _pager = new KalturaFilterPager({ pageSize: 25, pageIndex: 1 });
+  public _filter = new KalturaReportInputFilter({
+    searchInTags: true,
+    searchInAdminTags: false
+  });
+  
+  public get _isCompareMode(): boolean {
+    return this._compareFilter !== null;
+  }
+  
+  constructor(private _frameEventManager: FrameEventManagerService,
+              private _translate: TranslateService,
+              private _reportService: ReportService,
+              private _compareService: CompareService,
+              private _errorsManager: ErrorsManagerService,
+              private _authService: AuthService,
+              private _dataConfigService: VideoPerformanceConfig) {
+    super();
+
+    this._dataConfig = _dataConfigService.getConfig();
+  }
+  
+  protected _loadReport(sections = this._dataConfig): void {
+    this._isBusy = true;
+    this._blockerMessage = null;
+    
+    const reportConfig: ReportConfig = { reportType: this._reportType, filter: this._filter, pager: this._pager, order: this._order };
+    if (reportConfig['objectIds__null']) {
+      delete reportConfig['objectIds__null'];
+    }
+    reportConfig.objectIds = this.entryId;
+
+    this._reportService.getReport(reportConfig, sections)
+      .pipe(switchMap(report => {
+        if (!this._isCompareMode) {
+          return ObservableOf({ report, compare: null });
+        }
+        
+        const compareReportConfig = { reportType: this._reportType, filter: this._compareFilter, pager: this._pager, order: this._order };
+        return this._reportService.getReport(compareReportConfig, sections)
+          .pipe(map(compare => ({ report, compare })));
+      }))
+      .subscribe(({ report, compare }) => {
+          if (compare) {
+            this._handleCompare(report, compare);
+          } else {
+            if (report.totals) {
+              this._handleTotals(report.totals); // handle totals
+            }
+          }
+          this._isBusy = false;
+        },
+        error => {
+          this._isBusy = false;
+          const err: ErrorDetails = this._errorsManager.getError(error);
+          let buttons: AreaBlockerMessageButton[] = [];
+          if (err.forceLogout) {
+            buttons = [{
+              label: this._translate.instant('app.common.ok'),
+              action: () => {
+                this._blockerMessage = null;
+                this._authService.logout();
+              }
+            }];
+          } else {
+            buttons = [{
+              label: this._translate.instant('app.common.close'),
+              action: () => {
+                this._blockerMessage = null;
+              }
+            },
+              {
+                label: this._translate.instant('app.common.retry'),
+                action: () => {
+                  this._loadReport();
+                }
+              }];
+          }
+          this._blockerMessage = new AreaBlockerMessage({
+            title: err.title,
+            message: err.message,
+            buttons
+          });
+        });
+  }
+  
+  protected _updateRefineFilter(): void {
+    // this._refineFilterToServerValue(this._filter);
+    // if (this._compareFilter) {
+    //   this._refineFilterToServerValue(this._compareFilter);
+    // }
+  }
+  
+  protected _updateFilter(): void {
+    this._filter.timeZoneOffset = this._dateFilter.timeZoneOffset;
+    this._filter.fromDay = this._dateFilter.startDay;
+    this._filter.toDay = this._dateFilter.endDay;
+    this._filter.interval = this._dateFilter.timeUnits;
+    this._reportInterval = this._dateFilter.timeUnits;
+    this._pager.pageIndex = 1;
+    if (this._dateFilter.compare.active) {
+      const compare = this._dateFilter.compare;
+      this._compareFilter = Object.assign(KalturaObjectBaseFactory.createObject(this._filter), this._filter);
+      this._compareFilter.fromDay = compare.endDay;
+      this._compareFilter.toDay = this._dateFilter.endDay;
+    } else {
+      this._compareFilter = null;
+    }
+  }
+  
+  private _handleCompare(current: Report, compare: Report): void {
+    const currentPeriod = { from: this._filter.fromDay, to: this._filter.toDay };
+    const comparePeriod = { from: this._compareFilter.fromDay, to: this._compareFilter.toDay };
+    
+    if (current.totals && compare.totals) {
+      this._tabsData = this._compareService.compareTotalsData(
+        currentPeriod,
+        comparePeriod,
+        current.totals,
+        compare.totals,
+        this._dataConfig.totals
+      );
+    }
+  }
+
+  private _handleTotals(totals: KalturaReportTotal): void {
+    this._tabsData = this._reportService.parseTotals(totals, this._dataConfig.totals);
+  }
+
+}
