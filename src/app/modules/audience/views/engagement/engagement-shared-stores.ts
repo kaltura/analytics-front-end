30a5bb33ca143b2b66e6b5e1178cc6a9f5daf703
Stas <staskgt@gmail.com>
2019-02-13 13:01:29 +0200

    fix typings

--- src/app/modules/audience/views/engagement/engagement-shared-stores.ts
+++ src/app/modules/audience/views/engagement/engagement-shared-stores.ts
@@ -1,4 +1,4 @@
-import { InjectionToken } from '@angular/core';
+import { FactoryProvider, InjectionToken } from '@angular/core';
 import { SharedReportBaseStore } from 'shared/services/shared-report-base-store';
 import { ReportService } from 'shared/services';
 
@@ -6,7 +6,7 @@ export const HighlightsSharedStore = new InjectionToken('HighlightsSharedStore')
 
 export const TopVideosSharedStore = new InjectionToken('TopVideosSharedStore');
 
-export function provideSharedStore(token) {
+export function provideSharedStore(token: InjectionToken<string>): FactoryProvider {
   return {
     provide: token,
     useFactory: (reportService: ReportService) => new SharedReportBaseStore(reportService),
