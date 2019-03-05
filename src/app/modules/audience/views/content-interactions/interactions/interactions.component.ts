fa316f9a24ea77cfde56f1617fd15ce6b471943b
Stas Kohut <diamond-darrell@users.noreply.github.com>
2019-03-05 15:12:17 +0200

    Content interactions (#128)
    * wip
    
    * wip
    
    * fix typo
    
    * wip
    
    * wip
    
    * wip
    
    * remove redundant report
    
    * fix typo
    
    * fix pagination
    
    * content-interactions: updated icon

---
+++ src/app/modules/audience/views/content-interactions/interactions/interactions.component.ts
@@ -0,0 +1,259 @@
+import { Component, Input, OnDestroy } from '@angular/core';
+import { Tab } from 'shared/components/report-tabs/report-tabs.component';
+import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaObjectBaseFactory, KalturaReportGraph, KalturaReportInterval, KalturaReportTable, KalturaReportTotal, KalturaReportType } from 'kaltura-ngx-client';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
+import { AuthService, ErrorsManagerService, Report, ReportConfig, ReportService } from 'shared/services';
+import { map, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, of as ObservableOf } from 'rxjs';
+import { CompareService } from 'shared/services/compare.service';
+import { ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
+import { TranslateService } from '@ngx-translate/core';
+import { DateFilterComponent } from 'shared/components/date-filter/date-filter.component';
+import { FrameEventManagerService, FrameEvents } from 'shared/modules/frame-event-manager/frame-event-manager.service';
+import { isEmptyObject } from 'shared/utils/is-empty-object';
+import { KalturaLogger } from '@kaltura-ng/kaltura-logger';
+import { analyticsConfig } from 'configuration/analytics-config';
+import { TableRow } from 'shared/utils/table-local-sort-handler';
+import { InteractionsBaseReportComponent } from '../interactions-base-report/interactions-base-report.component';
+import { InteractionsConfig } from './interactions.config';
+import { SortEvent } from 'primeng/api';
+
+@Component({
+  selector: 'app-interactions',
+  templateUrl: './interactions.component.html',
+  styleUrls: ['./interactions.component.scss'],
+  providers: [
+    KalturaLogger.createLogger('InteractionsComponent'),
+    InteractionsConfig,
+    ReportService
+  ],
+})
+export class InteractionsComponent extends InteractionsBaseReportComponent implements OnDestroy {
+  @Input() dateFilterComponent: DateFilterComponent;
+  
+  private _order = '-count_viral';
+  private _reportType = KalturaReportType.contentInteractions;
+  private _dataConfig: ReportDataConfig;
+  private _partnerId = analyticsConfig.pid;
+  private _apiUrl = analyticsConfig.kalturaServer.uri.startsWith('http')
+    ? analyticsConfig.kalturaServer.uri
+    : `${location.protocol}//${analyticsConfig.kalturaServer.uri}`;
+  
+  protected _componentId = 'interactions';
+  
+  public interactions$ = new BehaviorSubject<{ current: Report, compare: Report, busy: boolean, error: AreaBlockerMessage }>({ current: null, compare: null, busy: false, error: null });
+  
+  public _columns: string[] = [];
+  public _firstTimeLoading = true;
+  public _isBusy = false;
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _tabsData: Tab[] = [];
+  public _tableData: TableRow[] = [];
+  public _selectedMetrics: string;
+  public _reportInterval = KalturaReportInterval.days;
+  public _compareFilter: KalturaEndUserReportInputFilter = null;
+  public _lineChartData = {};
+  public _showTable = false;
+  public _totalCount = 0;
+  public _pager = new KalturaFilterPager({ pageSize: analyticsConfig.defaultPageSize, pageIndex: 1 });
+  public _filter = new KalturaEndUserReportInputFilter({
+    searchInTags: true,
+    searchInAdminTags: false
+  });
+  
+  public get _isCompareMode(): boolean {
+    return this._compareFilter !== null;
+  }
+  
+  constructor(private _frameEventManager: FrameEventManagerService,
+              private _translate: TranslateService,
+              private _reportService: ReportService,
+              private _compareService: CompareService,
+              private _errorsManager: ErrorsManagerService,
+              private _authService: AuthService,
+              private _dataConfigService: InteractionsConfig,
+              private _logger: KalturaLogger) {
+    super();
+    
+    this._dataConfig = _dataConfigService.getConfig();
+    this._selectedMetrics = this._dataConfig.totals.preSelected;
+  }
+  
+  ngOnDestroy() {
+    this.interactions$.complete();
+  }
+  
+  protected _loadReport(sections = this._dataConfig): void {
+    this.interactions$.next({ current: null, compare: null, busy: true, error: null });
+    this._isBusy = true;
+    this._blockerMessage = null;
+    
+    const reportConfig: ReportConfig = { reportType: this._reportType, filter: this._filter, order: this._order, pager: this._pager };
+    this._reportService.getReport(reportConfig, sections)
+      .pipe(switchMap(report => {
+        if (!this._isCompareMode) {
+          return ObservableOf({ report, compare: null });
+        }
+        
+        const compareReportConfig = { reportType: this._reportType, filter: this._compareFilter, order: this._order, pager: this._pager };
+        
+        return this._reportService.getReport(compareReportConfig, sections)
+          .pipe(map(compare => ({ report, compare })));
+      }))
+      .subscribe(({ report, compare }) => {
+          this._tableData = [];
+          
+          this.interactions$.next({ current: report, compare: compare, busy: false, error: null });
+          
+          if (report.totals && !this._tabsData.length) {
+            this._handleTotals(report.totals); // handle totals
+          }
+          
+          if (compare) {
+            this._handleCompare(report, compare);
+          } else {
+            if (report.table && report.table.data && report.table.header) {
+              this._handleTable(report.table); // handle table
+            }
+            if (report.graphs.length) {
+              this._handleGraphs(report.graphs); // handle graphs
+            }
+          }
+          this._firstTimeLoading = false;
+          this._isBusy = false;
+        },
+        error => {
+          this._isBusy = false;
+          const actions = {
+            'close': () => {
+              this._blockerMessage = null;
+            },
+            'retry': () => {
+              this._loadReport();
+            },
+          };
+          this._blockerMessage = this._errorsManager.getErrorMessage(error, actions);
+          this.interactions$.next({ current: null, compare: null, busy: false, error: this._errorsManager.getErrorMessage(error, actions) });
+        });
+  }
+  
+  protected _updateFilter(): void {
+    this._filter.timeZoneOffset = this._dateFilter.timeZoneOffset;
+    this._filter.fromDate = this._dateFilter.startDate;
+    this._filter.toDate = this._dateFilter.endDate;
+    this._filter.interval = this._dateFilter.timeUnits;
+    this._reportInterval = this._dateFilter.timeUnits;
+    if (this._dateFilter.compare.active) {
+      const compare = this._dateFilter.compare;
+      this._compareFilter = Object.assign(KalturaObjectBaseFactory.createObject(this._filter), this._filter);
+      this._compareFilter.fromDate = compare.startDate;
+      this._compareFilter.toDate = compare.endDate;
+    } else {
+      this._compareFilter = null;
+    }
+  }
+  
+  protected _updateRefineFilter(): void {
+    this._refineFilterToServerValue(this._filter);
+    if (this._compareFilter) {
+      this._refineFilterToServerValue(this._compareFilter);
+    }
+  }
+  
+  private _handleCompare(current: Report, compare: Report): void {
+    const currentPeriod = { from: this._filter.fromDate, to: this._filter.toDate };
+    const comparePeriod = { from: this._compareFilter.fromDate, to: this._compareFilter.toDate };
+    
+    if (current.graphs.length && compare.graphs.length) {
+      const { lineChartData } = this._compareService.compareGraphData(
+        currentPeriod,
+        comparePeriod,
+        current.graphs,
+        compare.graphs,
+        this._dataConfig.graph,
+        this._reportInterval,
+      );
+      this._lineChartData = !isEmptyObject(lineChartData) ? lineChartData : null;
+    }
+    
+    if (current.table && compare.table) {
+      const { columns, tableData } = this._compareService.compareTableData(
+        currentPeriod,
+        comparePeriod,
+        current.table,
+        compare.table,
+        this._dataConfig.table,
+        this._reportInterval,
+      );
+      this._columns = columns;
+      this._totalCount = compare.table.totalCount;
+      this._tableData = tableData.map((item, index) => this._extendTableRow(item, index));
+    }
+  }
+  
+  private _extendTableRow (item: TableRow<string>, index: number): TableRow<string> {
+    item['index'] = String(this._pager.pageSize * (this._pager.pageIndex - 1) + (index + 1));
+    item['thumbnailUrl'] = `${this._apiUrl}/p/${this._partnerId}/sp/${this._partnerId}00/thumbnail/entry_id/${item['object_id']}/width/256/height/144?rnd=${Math.random()}`;
+    return item;
+  }
+  
+  private _handleTable(table: KalturaReportTable): void {
+    const { columns, tableData } = this._reportService.parseTableData(table, this._dataConfig.table);
+    this._totalCount = table.totalCount;
+    this._columns = columns;
+    this._tableData = tableData.map((item, index) => this._extendTableRow(item, index));
+  }
+  
+  private _handleTotals(totals: KalturaReportTotal): void {
+    this._tabsData = this._reportService.parseTotals(totals, this._dataConfig.totals, this._selectedMetrics);
+  }
+  
+  private _handleGraphs(graphs: KalturaReportGraph[]): void {
+    const { lineChartData } = this._reportService.parseGraphs(
+      graphs,
+      this._dataConfig.graph,
+      { from: this._filter.fromDate, to: this._filter.toDate },
+      this._reportInterval,
+    );
+    
+    this._lineChartData = !isEmptyObject(lineChartData) ? lineChartData : null;
+  }
+  
+  public _onTabChange(tab: Tab): void {
+    this._logger.trace('Handle tab change action by user', { tab });
+    this._selectedMetrics = tab.key;
+  }
+  
+  public _toggleTable(): void {
+    this._logger.trace('Handle toggle table visibility action by user', { tableVisible: !this._showTable });
+    this._showTable = !this._showTable;
+    
+    if (analyticsConfig.isHosted) {
+      setTimeout(() => {
+        const height = document.getElementById('analyticsApp').getBoundingClientRect().height;
+        this._logger.trace('Send update layout event to the host app', { height });
+        this._frameEventManager.publish(FrameEvents.UpdateLayout, { height });
+      }, 0);
+    }
+  }
+  
+  public _onPaginationChanged(event: { page: number, pageCount: number, rows: TableRow<string>, first: number }): void {
+    if (event.page !== (this._pager.pageIndex - 1)) {
+      this._logger.trace('Handle pagination changed action by user', { newPage: event.page + 1 });
+      this._pager.pageIndex = event.page + 1;
+      this._loadReport({ table: this._dataConfig.table });
+    }
+  }
+  
+  public _onSortChanged(event: SortEvent): void {
+    if (event.data.length && event.field && event.order) {
+      const order = event.order === 1 ? '+' + event.field : '-' + event.field;
+      if (order !== this._order) {
+        this._logger.trace('Handle sort changed action by user, reset page index to 1', { order });
+        this._order = order;
+        this._pager.pageIndex = 1;
+        this._loadReport({ table: this._dataConfig.table });
+      }
+    }
+  }
+}
