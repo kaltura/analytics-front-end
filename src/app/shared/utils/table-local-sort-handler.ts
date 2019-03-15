6f4edd4dcd7ef9f3d30d8a54ba45986d9149d9c2
Stas <staskgt@gmail.com>
2019-03-15 10:43:36 +0200

    fix date for ie

--- src/app/shared/utils/table-local-sort-handler.ts
+++ src/app/shared/utils/table-local-sort-handler.ts
@@ -3,6 +3,7 @@ import { SortEvent } from 'primeng/api';
 export type TableRow<T = any> = { [key: string]: T };
 
 const dateKeys = ['month_id', 'date_id'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export function tableLocalSortHandler(event: SortEvent, initialOrder: string = null, isCompareMode = false): string {
   if (event.data.length && event.field && event.order && !isCompareMode) {
@@ -14,8 +15,8 @@ export function tableLocalSortHandler(event: SortEvent, initialOrder: string = n
         let result = 0;
     
         if (dateKeys.indexOf(event.field) !== -1) {
-          value1 = new Date(value1);
-          value2 = new Date(value2);
+          value1 = getDate(value1);
+          value2 = getDate(value2);
         }
     
         if (typeof value1 === 'string' && typeof value2 === 'string') {
@@ -33,4 +34,21 @@ export function tableLocalSortHandler(event: SortEvent, initialOrder: string = n
   }
   
   return initialOrder;
+}
+
+// parse date assuming there're 2 formats: 'MonthName Year' and 'MM/DD/YYYY'
+// first one is not supported by IE
+// create custom date from this format, otherwise fallback to standard approach
+function getDate(date: string): Date {
+  if (typeof date !== 'string') {
+    return null;
+  }
+  
+  const [month, year] = date.split(' ');
+  const monthIndex = months.indexOf(month) + 1;
+  if (monthIndex) {
+    return new Date(`${monthIndex}/01/${year}`);
+  }
+  
+  return new Date(date);
 }
