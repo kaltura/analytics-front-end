d891d5fca578d1fc3abc3517ecd6af2e7566768e
Stas <staskgt@gmail.com>
2019-04-02 15:47:13 +0300

    fix incorrect total size

--- src/app/modules/audience/views/content-interactions/interactions/interactions.component.ts
+++ src/app/modules/audience/views/content-interactions/interactions/interactions.component.ts
@@ -147,6 +147,7 @@ export class InteractionsComponent extends InteractionsBaseReportComponent imple
     this._filter.toDate = this._dateFilter.endDate;
     this._filter.interval = this._dateFilter.timeUnits;
     this._reportInterval = this._dateFilter.timeUnits;
+    this._pager.pageIndex = 1;
     if (this._dateFilter.compare.active) {
       const compare = this._dateFilter.compare;
       this._compareFilter = Object.assign(KalturaObjectBaseFactory.createObject(this._filter), this._filter);
@@ -158,6 +159,8 @@ export class InteractionsComponent extends InteractionsBaseReportComponent imple
   }
   
   protected _updateRefineFilter(): void {
+    this._pager.pageIndex = 1;
+
     this._refineFilterToServerValue(this._filter);
     if (this._compareFilter) {
       this._refineFilterToServerValue(this._compareFilter);
@@ -191,7 +194,7 @@ export class InteractionsComponent extends InteractionsBaseReportComponent imple
         'object_id'
       );
       this._columns = columns;
-      this._totalCount = compare.table.totalCount;
+      this._totalCount = current.table.totalCount;
       this._tableData = tableData.map((item, index) => this._extendTableRow(item, index));
     }
   }
