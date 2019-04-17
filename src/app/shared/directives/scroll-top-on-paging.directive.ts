ae6e2d57c99ff602804bf5aad42badfc6590eb24
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-04-17 13:45:43 +0300

    fix scroll calculation

--- src/app/shared/directives/scroll-top-on-paging.directive.ts
+++ src/app/shared/directives/scroll-top-on-paging.directive.ts
@@ -40,35 +40,22 @@ export class ScrollTopOnPagingDirective implements OnInit, AfterViewInit, OnDest
       const sources = this.customPaginationEvent ? [this._table.onPage, this.customPaginationEvent] : [this._table.onPage];
       ObservableMerge(...sources)
         .pipe(cancelOnDestroy(this))
-        .subscribe(() => this._scrollTop2());
+        .subscribe(() => this._scrollTop());
     }
-  }
-  
-  private _scrollTop2(): void {
-    const currentScrollTop = window.scrollY;
-    const currentTableHeight = this._tableElement.getBoundingClientRect().height;
-    setTimeout(() => {
-      const deltaHeight = currentTableHeight - this._tableElement.getBoundingClientRect().height;
-      if (deltaHeight > 0) {
-        window.scrollTo(0, currentScrollTop - deltaHeight);
-      }
-    }, 0);
   }
   
   private _scrollTop(): void {
-    if (analyticsConfig.isHosted) {
-      const targetEl = this._tableElement.getElementsByClassName('ui-table')[0] as HTMLElement;
-      
-      if (targetEl) {
-        const rect = targetEl.getBoundingClientRect();
-        const top = rect.top + window.pageYOffset;
-        
-        this._frameEventManager.publish(FrameEvents.ScrollTo, top - this.scrollOffset);
-      }
-    } else {
-      PageScrollConfig.defaultDuration = 0;
-      const pageScrollInstance = PageScrollInstance.simpleInstance(document, this._tableElement);
-      this._pageScrollService.start(pageScrollInstance);
-    }
+    try {
+      const win = analyticsConfig.isHosted ? window.parent : window;
+      const currentScrollTop = win.pageYOffset;
+      const currentTableHeight = this._tableElement.getBoundingClientRect().height;
+      setTimeout(() => {
+        const deltaHeight = currentTableHeight - this._tableElement.getBoundingClientRect().height;
+        if (deltaHeight > 0 && win) {
+          win.scrollTo(0, currentScrollTop - deltaHeight);
+        }
+      }, 0);
+    } catch (e) {}
   }
+
 }
