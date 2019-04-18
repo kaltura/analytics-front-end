956fbe5ced36b68430f1cfceb6f3ff2bbe9f322f
Stas <staskgt@gmail.com>
2019-04-18 18:29:51 +0300

    wip

--- src/app/modules/entry-live/views/entry-details/entry-details.component.ts
+++ src/app/modules/entry-live/views/entry-details/entry-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { KalturaLiveEntry } from 'kaltura-ngx-client';
+import { KalturaDVRStatus, KalturaLiveEntry, KalturaRecordingStatus } from 'kaltura-ngx-client';
 
 @Component({
   selector: 'app-entry-details',
@@ -10,4 +10,7 @@ export class EntryDetailsComponent {
   @Input() entry: KalturaLiveEntry;
 
   @Output() navigateToEntry = new EventEmitter<void>();
+  
+  public _dvrStatus = KalturaDVRStatus;
+  public _recordingStatus = KalturaRecordingStatus;
 }
