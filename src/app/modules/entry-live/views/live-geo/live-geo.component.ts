33abf9409655ade5c6d0c26a63e757ff882fcfbc
Stas <staskgt@gmail.com>
2019-05-14 09:51:01 +0300

    wip

--- src/app/modules/entry-live/views/live-geo/live-geo.component.ts
+++ src/app/modules/entry-live/views/live-geo/live-geo.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaReportInterval, KalturaReportTable, KalturaReportTotal, KalturaReportType } from 'kaltura-ngx-client';
-import { AuthService, ErrorsManagerService, ReportConfig, ReportHelper, ReportService } from 'shared/services';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { KalturaEndUserReportInputFilter, KalturaReportInterval, KalturaReportType } from 'kaltura-ngx-client';
+import { ErrorsManagerService, ReportConfig } from 'shared/services';
 import { ReportDataConfig } from 'shared/services/storage-data-base.config';
 import { TranslateService } from '@ngx-translate/core';
 import { DateChangeEvent } from 'shared/components/date-filter/date-filter.service';
@@ -8,13 +8,12 @@ import { TableRow } from 'shared/utils/table-local-sort-handler';
 import { Tab } from 'shared/components/report-tabs/report-tabs.component';
 import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
 import { RefineFilter } from 'shared/components/filter/filter.component';
-import { TrendService } from 'shared/services/trend.service';
 import { HttpClient } from '@angular/common/http';
 import { refineFilterToServerValue } from 'shared/components/filter/filter-to-server-value.util';
 import { cancelOnDestroy } from '@kaltura-ng/kaltura-common';
-import { significantDigits } from 'shared/utils/significant-digits';
 import { LiveGeoConfig } from './live-geo.config';
 import { getCountryName } from 'shared/utils/get-country-name';
+import * as echarts from 'echarts';
 import { EChartOption } from 'echarts';
 import { canDrillDown } from 'shared/utils/can-drill-down-country';
 import { DataTable } from 'primeng/primeng';
@@ -27,14 +26,10 @@ import { LiveGeoWidget } from './live-geo.widget';
   styleUrls: ['./live-geo.component.scss'],
 })
 export class LiveGeoComponent implements OnInit, OnDestroy {
-  @Input() entryId = '';
-  
   @ViewChild('table') _table: DataTable;
   
   private _dataConfig: ReportDataConfig;
   private _mapCenter = [0, 10];
-  private _order = '-count_plays';
-  private _pager: KalturaFilterPager = new KalturaFilterPager({ pageSize: 500, pageIndex: 1 });
   private _echartsIntance: any; // echart instance
   private _canMapDrillDown = true;
   
@@ -53,13 +48,9 @@ export class LiveGeoComponent implements OnInit, OnDestroy {
   public _reportType = KalturaReportType.mapOverlayCountry;
   public _filter = new KalturaEndUserReportInputFilter({ searchInTags: true, searchInAdminTags: false });
   public _drillDown: string[] = [];
-  public _mapData: any;
   
   constructor(private _translate: TranslateService,
               private _errorsManager: ErrorsManagerService,
-              private _reportService: ReportService,
-              private _authService: AuthService,
-              private _trendService: TrendService,
               private _http: HttpClient,
               private _liveGeoWidget: LiveGeoWidget,
               private _dataConfigService: LiveGeoConfig) {
@@ -75,9 +66,10 @@ export class LiveGeoComponent implements OnInit, OnDestroy {
     // load works map data
     this._http.get('assets/world.json')
       .subscribe(data => {
-        this._mapData = data;
+        echarts.registerMap('world', data);
+        this._mapDataReady = true;
       });
-  
+    
     this._liveGeoWidget.state$
       .pipe(cancelOnDestroy(this))
       .subscribe(state => {
@@ -94,57 +86,17 @@ export class LiveGeoComponent implements OnInit, OnDestroy {
           this._blockerMessage = this._errorsManager.getErrorMessage(state.error, actions);
         }
       });
-  
+    
     this._liveGeoWidget.data$
       .pipe(cancelOnDestroy(this), filter(Boolean))
       .subscribe(data => {
         this._isBusy = false;
         this._setMapCenter();
+        setTimeout(() => {
+          this._updateMap(this._mapCenter);
+        }, 0);
         console.warn(data);
       });
-  }
-  
-  protected _loadReport(sections = this._dataConfig): void {
-    this._isBusy = true;
-    this._setMapCenter();
-    this._tableData = [];
-    this._blockerMessage = null;
-    
-    if (this.entryId) {
-      this._filter.entryIdIn = this.entryId;
-    }
-    
-    const reportConfig: ReportConfig = { reportType: this._reportType, filter: this._filter, pager: this._pager, order: this._order };
-    this._updateReportConfig(reportConfig);
-    this._reportService.getReport(reportConfig, sections)
-      .pipe(cancelOnDestroy(this))
-      .subscribe(report => {
-          this._isBusy = false;
-          
-          if (report.totals) {
-            this._tabsData = this._handleTotals(report.totals); // handle totals
-          }
-          
-          if (report.table && report.table.header && report.table.data) {
-            this._tableData = this._handleTable(report.table, this._tabsData); // handle table
-          }
-          
-          setTimeout(() => {
-            this._updateMap(this._mapCenter);
-          }, 0);
-        },
-        error => {
-          this._isBusy = false;
-          const actions = {
-            'close': () => {
-              this._blockerMessage = null;
-            },
-            'retry': () => {
-              this._loadReport();
-            },
-          };
-          this._blockerMessage = this._errorsManager.getErrorMessage(error, actions);
-        });
   }
   
   private _updateMap(mapCenter: number[]): void {
@@ -176,36 +128,6 @@ export class LiveGeoComponent implements OnInit, OnDestroy {
     map.zoom = this._mapZoom;
     map.roam = this._drillDown.length === 0 && this._canMapDrillDown ? 'false' : 'move';
     this._mapChartData = mapConfig;
-  }
-  
-  private _handleTable(table: KalturaReportTable, tabsData: Tab[]): TableRow[] {
-    const { columns, tableData } = this._reportService.parseTableData(table, this._dataConfig.table);
-    this._columns = columns;
-    this._columns[0] = this._columns.splice(1, 1, this._columns[0])[0]; // switch places between the first 2 columns
-    this._columns.unshift('index'); // add line indexing column at the beginning
-    let tmp = this._columns.pop();
-    this._columns.push('distribution'); // add distribution column at the end
-    this._columns.push(tmp);
-    
-    return tableData.map((row, index) => {
-      const calculateDistribution = (key: string): number => {
-        const tab = tabsData.find(item => item.key === key);
-        const total = tab ? parseFloat((tab.rawValue as string).replace(new RegExp(',', 'g'), '')) : 0;
-        const rowValue = row[key] ? parseFloat((row[key] as string).replace(new RegExp(',', 'g'), '')) : 0;
-        return significantDigits((rowValue / total) * 100);
-      };
-      const playsDistribution = calculateDistribution('count_plays');
-      const usersDistribution = calculateDistribution('unique_known_users');
-      
-      row['plays_distribution'] = ReportHelper.numberWithCommas(playsDistribution);
-      row['unique_users_distribution'] = ReportHelper.numberWithCommas(usersDistribution);
-      
-      return row;
-    });
-  }
-  
-  private _handleTotals(totals: KalturaReportTotal): Tab[] {
-    return this._reportService.parseTotals(totals, this._dataConfig.totals, this._selectedMetrics);
   }
   
   private _updateReportConfig(reportConfig: ReportConfig): void {
@@ -283,7 +205,7 @@ export class LiveGeoComponent implements OnInit, OnDestroy {
     this._reportType = this._drillDown.length === 2 ? KalturaReportType.mapOverlayCity : this._drillDown.length === 1 ? KalturaReportType.mapOverlayRegion : KalturaReportType.mapOverlayCountry;
     
     if (reload) {
-      this._loadReport();
+      // this._loadReport();
     }
   }
   
