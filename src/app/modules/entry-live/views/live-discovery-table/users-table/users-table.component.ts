9a53b7a8148861bf3f0088230ddaac8706551e67
Stas <staskgt@gmail.com>
2019-06-07 09:28:58 +0300

    wip

--- src/app/modules/entry-live/views/live-discovery-table/users-table/users-table.component.ts
+++ src/app/modules/entry-live/views/live-discovery-table/users-table/users-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SortEvent } from 'primeng/api';
 import { KalturaFilterPager } from 'kaltura-ngx-client';
 import { TableRow } from 'shared/utils/table-local-sort-handler';
@@ -17,6 +17,8 @@ export class UsersTableComponent {
   @Input() totalCount = 0;
   @Input() firstTimeLoading = true;
   
+  private _initialSortEvent = true;
+  
   public _order = '-avg_view_buffering';
   public _pager = new KalturaFilterPager({ pageSize: liveDiscoveryTablePageSize, pageIndex: 1 });
   
@@ -31,6 +33,11 @@ export class UsersTableComponent {
   }
   
   public _onSortChanged(event: SortEvent): void {
+    if (this._initialSortEvent) { // suppress first sort event to prevent emitting wrong order
+      this._initialSortEvent = false;
+      return;
+    }
+
     if (event.data.length && event.field && event.order) {
       const order = event.order === 1 ? '+' + event.field : '-' + event.field;
       if (order !== this._order) {
