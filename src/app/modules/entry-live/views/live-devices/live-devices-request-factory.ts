cd6c81fe0b2bb883b3fd4e08fa6118dfd5043067
Stas <staskgt@gmail.com>
2019-06-18 12:16:49 +0300

    calculate devices total on the client side

--- src/app/modules/entry-live/views/live-devices/live-devices-request-factory.ts
+++ src/app/modules/entry-live/views/live-devices/live-devices-request-factory.ts
@@ -1,5 +1,5 @@
 import { RequestFactory } from '@kaltura-ng/kaltura-common';
-import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaMultiRequest, KalturaMultiResponse, KalturaReportResponseOptions, KalturaReportType, ReportGetTableAction, ReportGetTableActionArgs, ReportGetTotalAction, ReportGetTotalActionArgs } from 'kaltura-ngx-client';
+import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaMultiRequest, KalturaMultiResponse, KalturaReportResponseOptions, KalturaReportType, ReportGetTableAction, ReportGetTableActionArgs } from 'kaltura-ngx-client';
 import { analyticsConfig } from 'configuration/analytics-config';
 import * as moment from 'moment';
 import { OnPollTickSuccess } from 'shared/services/server-polls-base.service';
@@ -21,35 +21,22 @@ export class LiveDevicesRequestFactory implements RequestFactory<KalturaMultiReq
     responseOptions: this._responseOptions
   };
   
-  private _getTotalsActionArgs: ReportGetTotalActionArgs = {
-    reportType: KalturaReportType.platformsRealtime,
-    reportInputFilter: new KalturaEndUserReportInputFilter({
-      toDate: moment().unix(),
-      fromDate: this._getFromDate(),
-    }),
-    responseOptions: this._responseOptions,
-  };
-  
   private _getFromDate(): number {
     return moment().subtract(3, 'hours').unix();
   }
   
   constructor(private _entryId: string) {
     this._getTableActionArgs.reportInputFilter.entryIdIn = this._entryId;
-    this._getTotalsActionArgs.reportInputFilter.entryIdIn = this._entryId;
   }
   
   public onPollTickSuccess(): void {
     this._getTableActionArgs.reportInputFilter.toDate = moment().unix();
     this._getTableActionArgs.reportInputFilter.fromDate = this._getFromDate();
-    this._getTotalsActionArgs.reportInputFilter.toDate = moment().unix();
-    this._getTotalsActionArgs.reportInputFilter.fromDate = this._getFromDate();
   }
   
   public create(): KalturaMultiRequest {
     return new KalturaMultiRequest(
       new ReportGetTableAction(this._getTableActionArgs),
-      new ReportGetTotalAction(this._getTotalsActionArgs),
     );
   }
 }
