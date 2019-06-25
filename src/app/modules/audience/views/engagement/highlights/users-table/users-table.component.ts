2173b1848fcc99023d34185901abe5fe6de06e73
Stas <staskgt@gmail.com>
2019-06-25 11:37:00 +0300

    wip

--- src/app/modules/audience/views/engagement/highlights/users-table/users-table.component.ts
+++ src/app/modules/audience/views/engagement/highlights/users-table/users-table.component.ts
@@ -1,15 +1,131 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
+import { ErrorsManagerService, Report, ReportConfig, ReportService } from 'shared/services';
+import { map, switchMap } from 'rxjs/operators';
+import { of as ObservableOf } from 'rxjs';
+import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaReportInterval, KalturaReportTable, KalturaReportType } from 'kaltura-ngx-client';
+import { TableRow } from 'shared/utils/table-local-sort-handler';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
+import { UsersTableConfig } from './users-table.config';
+import { CompareService } from 'shared/services/compare.service';
+import { analyticsConfig } from 'configuration/analytics-config';
+import { SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
-  styleUrls: ['./users-table.component.scss']
+  styleUrls: ['./users-table.component.scss'],
+  providers: [UsersTableConfig],
 })
 export class UsersTableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() isCompareMode: boolean;
+  @Input() filter: KalturaEndUserReportInputFilter;
+  @Input() compareFilter: KalturaEndUserReportInputFilter;
+  @Input() reportInterval: KalturaReportInterval;
+  @Input() firstTimeLoading: boolean;
+  
+  private _reportType = KalturaReportType.userTopContent;
+  private _dataConfig: ReportDataConfig;
+  private _order = '-count_plays';
+  
+  public _tableData: TableRow[] = [];
+  public _columns: string[] = [];
+  public _totalCount = 0;
+  public _pager = new KalturaFilterPager({ pageIndex: 1, pageSize: analyticsConfig.defaultPageSize });
+  public _isBusy = true;
+  public _blockerMessage: AreaBlockerMessage = null;
+  
+  constructor(private _reportService: ReportService,
+              private _compareService: CompareService,
+              private _errorsManager: ErrorsManagerService,
+              private _dataConfigService: UsersTableConfig) {
+    this._dataConfig = _dataConfigService.getConfig();
   }
-
+  
+  ngOnInit() {
+    this.loadReport();
+  }
+  
+  public loadReport(): void {
+    this._isBusy = true;
+    const reportConfig: ReportConfig = { reportType: this._reportType, filter: this.filter, order: this._order };
+    this._reportService.getReport(reportConfig, this._dataConfig)
+      .pipe(switchMap(report => {
+        if (!this.isCompareMode) {
+          return ObservableOf({ report, compare: null });
+        }
+        
+        const compareReportConfig = { reportType: this._reportType, filter: this.compareFilter, order: this._order };
+        
+        return this._reportService.getReport(compareReportConfig, this._dataConfig)
+          .pipe(map(compare => ({ report, compare })));
+      }))
+      .subscribe(({ report, compare }) => {
+        console.warn(report);
+          this._tableData = [];
+          
+          if (compare) {
+            this._handleCompare(report, compare);
+          } else if (report.table && report.table.data && report.table.header) {
+            this._handleTable(report.table); // handle graphs
+          }
+          this._isBusy = false;
+          this._blockerMessage = null;
+        },
+        error => {
+          this._isBusy = false;
+          const actions = {
+            'close': () => {
+              this._blockerMessage = null;
+            },
+            'retry': () => {
+              this.loadReport();
+            },
+          };
+          this._blockerMessage = this._errorsManager.getErrorMessage(error, actions);
+        });
+  }
+  
+  private _handleCompare(current: Report, compare: Report): void {
+    const currentPeriod = { from: this.filter.fromDate, to: this.filter.toDate };
+    const comparePeriod = { from: this.compareFilter.fromDate, to: this.compareFilter.toDate };
+    
+    if (current.table && compare.table) {
+      const { columns, tableData } = this._compareService.compareTableData(
+        currentPeriod,
+        comparePeriod,
+        current.table,
+        compare.table,
+        this._dataConfig.table,
+        this.reportInterval,
+      );
+      this._columns = columns;
+      this._totalCount = compare.table.totalCount;
+      this._tableData = tableData;
+    }
+  }
+  
+  private _handleTable(table: KalturaReportTable): void {
+    const { columns, tableData } = this._reportService.parseTableData(table, this._dataConfig.table);
+    this._totalCount = table.totalCount;
+    this._columns = columns;
+    this._tableData = tableData;
+  }
+  
+  public _onPaginationChanged(event: { page: number }): void {
+    if (event.page !== (this._pager.pageIndex - 1)) {
+      this._pager.pageIndex = event.page + 1;
+      this.loadReport();
+    }
+  }
+  
+  public _onSortChanged(event: SortEvent): void {
+    if (event.data.length && event.field && event.order && !this.isCompareMode) {
+      const order = event.order === 1 ? '+' + event.field : '-' + event.field;
+      if (order !== this._order) {
+        this._order = order;
+        this.loadReport();
+      }
+    }
+  }
 }
