5f152a95e96f329893f9560fb1edc80845aa2816
Stas <staskgt@gmail.com>
2019-07-12 12:12:26 +0300

    wip

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { analyticsConfig } from 'configuration/analytics-config';
 import { cancelOnDestroy } from '@kaltura-ng/kaltura-common';
+import { filter } from 'rxjs/operators';
+
+export interface MenuItem {
+  id: string;
+  link: string;
+  label: string;
+  items?: MenuItem[];
+}
 
 @Component({
   selector: 'app-menu',
@@ -9,37 +17,101 @@ import { cancelOnDestroy } from '@kaltura-ng/kaltura-common';
   styleUrls: ['./app-menu.component.scss']
 })
 export class AppMenuComponent implements OnDestroy {
-  public activeRoute = '';
-  public activeSubRoute = '';
+  public _activeRoute = '';
+  public _activeSubRoute = '';
   
-  public get showNavBar(): boolean {
+  public _menu: MenuItem[] = [
+    {
+      id: 'audience',
+      link: 'audience',
+      label: 'app.modules.audience',
+      items: [
+        {
+          id: 'engagement',
+          link: 'audience/engagement',
+          label: 'app.audience.menu.engagement',
+        },
+        {
+          id: 'content-interactions',
+          link: 'audience/content-interactions',
+          label: 'app.audience.menu.contentInteractions',
+        },
+        {
+          id: 'technology',
+          link: 'audience/technology',
+          label: 'app.audience.menu.technology',
+        },
+        {
+          id: 'geo-location',
+          link: 'audience/geo-location',
+          label: 'app.audience.menu.geoLocation',
+        },
+      ]
+    },
+    {
+      id: 'contributors',
+      link: 'contributors',
+      label: 'app.modules.contributors',
+    },
+    {
+      id: 'bandwidth',
+      link: 'bandwidth',
+      label: 'app.modules.bandwidth',
+      items: [
+        {
+          id: 'publisher',
+          link: 'bandwidth/publisher',
+          label: 'app.bandwidth.publisherStorage',
+        },
+        {
+          id: 'end-user',
+          link: 'bandwidth/end-user',
+          label: 'app.bandwidth.usersStorage',
+        },
+      ]
+    },
+    {
+      id: 'live',
+      link: 'live',
+      label: 'app.modules.audience',
+    },
+  ];
+  
+  public get _showNavBar(): boolean {
     return analyticsConfig.showNavBar;
   }
-
+  
   constructor(private _router: Router) {
     _router.events
-      .pipe(cancelOnDestroy(this))
-      .subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          this.setSelectedRoute(event.urlAfterRedirects);
-        }
+      .pipe(cancelOnDestroy(this), filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.setSelectedRoute(event.urlAfterRedirects);
       });
   }
-
+  
+  ngOnDestroy() {
+  }
+  
   private setSelectedRoute(path: string): void {
     const urlTree = this._router.parseUrl(path);
     if (urlTree.root.children['primary']) {
       const [activeRoute, activeSubRoute] = urlTree.root.children['primary'].segments.map(({ path }) => path);
-      this.activeRoute = activeRoute || '';
-      this.activeSubRoute = activeSubRoute || '';
+      this._activeRoute = activeRoute || '';
+      this._activeSubRoute = activeSubRoute || '';
     }
   }
-  public navigateToView(route: string): void {
-    // TODO add smart navigation according to permissions
+  
+  public _navigateToView(route: string): void {
     this._router.navigate([route]);
   }
-
-  ngOnDestroy() {
+  
+  public _getSubMenu(id: string): MenuItem[] {
+    const relevantMenu = this._menu.find(item => item.id === id);
+    
+    if (relevantMenu) {
+      return relevantMenu.items || null;
+    }
+    
+    return null;
   }
-
 }
