62ce4c637b66b7c17bedfc3989278c124d8eb2cd
Stas <staskgt@gmail.com>
2019-08-23 12:57:29 +0300

    add most plays insight component with real data

--- src/app/modules/user/views/user-insight-plays/user-insight-plays.component.ts
+++ src/app/modules/user/views/user-insight-plays/user-insight-plays.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnDestroy } from '@angular/core';
 import { of as ObservableOf } from 'rxjs';
-import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaObjectBaseFactory, KalturaReportInterval, KalturaReportTable, KalturaReportType } from 'kaltura-ngx-client';
-import { ReportDataConfig } from 'shared/services/storage-data-base.config';
+import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaObjectBaseFactory, KalturaReport, KalturaReportGraph, KalturaReportInterval, KalturaReportType } from 'kaltura-ngx-client';
+import { ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
 import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
 import { FrameEventManagerService } from 'shared/modules/frame-event-manager/frame-event-manager.service';
 import { TranslateService } from '@ngx-translate/core';
-import { ErrorsManagerService, ReportConfig, ReportService } from 'shared/services';
+import { ErrorsManagerService, ReportConfig, ReportHelper, ReportService } from 'shared/services';
 import { UserBase } from '../user-base/user-base';
 import { UserInsightPlaysConfig } from './user-insight-plays.config';
 import { map, switchMap } from 'rxjs/operators';
@@ -28,8 +28,8 @@ export class UserInsightPlaysComponent extends UserBase implements OnDestroy {
   
   protected _componentId = 'insight-plays';
   private _dataConfig: ReportDataConfig;
-  private _reportType = reportTypeMap(KalturaReportType.topSyndication);
-  private _order = '-avg_time_viewed';
+  private _reportType = reportTypeMap(KalturaReportType.userHighlights);
+  private _order = '-date_id';
   
   public _compareFilter: KalturaEndUserReportInputFilter = null;
   public _isBusy: boolean;
@@ -63,6 +63,10 @@ export class UserInsightPlaysComponent extends UserBase implements OnDestroy {
     
     this._filter.userIds = this.userId;
     const reportConfig: ReportConfig = { reportType: this._reportType, filter: this._filter, order: this._order, pager: this._pager };
+    
+    sections = { ...sections }; // make local copy
+    delete sections[ReportDataSection.table]; // remove table config to prevent table request
+    
     this._reportService.getReport(reportConfig, sections)
       .pipe(switchMap(report => {
         if (!this._isCompareMode) {
@@ -76,19 +80,14 @@ export class UserInsightPlaysComponent extends UserBase implements OnDestroy {
           .pipe(map(compare => ({ report, compare })));
       }))
       .subscribe(({ report, compare }) => {
-          if (this._isCompareMode) {
-            this._currentDates = DateFilterUtils.getMomentDate(this._dateFilter.startDate).format('MMM DD, YYYY');
-            this._compareDates = DateFilterUtils.getMomentDate(this._dateFilter.compare.startDate).format('MMM DD, YYYY');
+          if (compare) {
+            this._handleCompare(report.graphs, compare.graphs);
           } else {
-            this._currentDates = DateFilterUtils.getMomentDate(new Date()).format('MMM DD, YYYY');
-            this._compareDates = DateFilterUtils.getMomentDate(new Date(2019, 3, 28)).format('MMM DD, YYYY');
+            if (report.graphs && report.graphs.length) {
+              this._handleTable(report.graphs);
+            }
           }
-    
-          this._tableData = [
-            { 'count_plays': 10, 'sum_time_viewed': 40, 'votes': 1, 'added_entries': 3 },
-            { 'count_plays': 1, 'sum_time_viewed': 2, 'votes': 3, 'added_entries': 0 },
-          ];
-          
+
           this._isBusy = false;
         },
         error => {
@@ -130,7 +129,54 @@ export class UserInsightPlaysComponent extends UserBase implements OnDestroy {
     }
   }
   
-  private _handleTable(table: KalturaReportTable): void {
+  private _maxPlaysRow(tableData: TableRow[]): TableRow {
+    return tableData.reduce((prev, current) => (prev['count_plays'] > current['count_plays']) ? prev : current);
+  }
   
+  private _handleCompare(current: KalturaReportGraph[], compare: KalturaReportGraph[]): void {
+    const { tableData } = this._reportService.tableFromGraph(
+      current,
+      this._dataConfig.table,
+      this._reportInterval,
+    );
+    const { tableData: compareTableData } = this._reportService.tableFromGraph(
+      compare,
+      this._dataConfig.table,
+      this._reportInterval,
+    );
+
+    const currentMaxPlays = this._maxPlaysRow(tableData);
+    const compareMaxPlays = this._maxPlaysRow(compareTableData);
+    currentMaxPlays['count_plays'] = ReportHelper.numberOrZero(currentMaxPlays['count_plays']);
+    compareMaxPlays['count_plays'] = ReportHelper.numberOrZero(compareMaxPlays['count_plays']);
+  
+    this._tableData = [currentMaxPlays, compareMaxPlays];
+    this._currentDates = currentMaxPlays['date_id'];
+    this._compareDates = compareMaxPlays['date_id'];
+  }
+  
+  private _handleTable(graphs: KalturaReportGraph[]): void {
+    const { tableData } = this._reportService.tableFromGraph(
+      graphs,
+      this._dataConfig.table,
+      this._reportInterval,
+    );
+    const maxPlaysRow = this._maxPlaysRow(tableData);
+    
+    // find last day with plays
+    let lastPlaysDay = null;
+    for (let i = tableData.length - 1; i >= 0; i--) {
+      if (tableData[i]['count_plays'] !== 0) {
+        lastPlaysDay = tableData[i];
+        break;
+      }
+    }
+  
+    lastPlaysDay['count_plays'] = ReportHelper.numberOrZero(lastPlaysDay['count_plays']);
+    maxPlaysRow['count_plays'] = ReportHelper.numberOrZero(maxPlaysRow['count_plays']);
+
+    this._tableData = [ lastPlaysDay, maxPlaysRow];
+    this._currentDates = lastPlaysDay['date_id'];
+    this._compareDates = maxPlaysRow['date_id'];
   }
 }
--- src/app/modules/user/views/user-insight-plays/user-insight-plays.config.ts
+++ src/app/modules/user/views/user-insight-plays/user-insight-plays.config.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
-import { EChartOption } from 'echarts';
-import { getPrimaryColor, getSecondaryColor } from 'shared/utils/colors';
 import { ReportHelper } from 'shared/services';
-import { TrendService } from 'shared/services/trend.service';
-import { TrendPipe } from 'shared/pipes/trend.pipe';
+import { DateFilterUtils } from 'shared/components/date-filter/date-filter-utils';
 
 @Injectable()
 export class UserInsightPlaysConfig extends ReportDataBaseConfig {
-  private _trendIcon = new TrendPipe();
-  constructor(_translate: TranslateService,
-              private _trendService: TrendService) {
+  constructor(_translate: TranslateService) {
     super(_translate);
   }
   
@@ -19,101 +14,26 @@ export class UserInsightPlaysConfig extends ReportDataBaseConfig {
     return {
       [ReportDataSection.table]: {
         fields: {
-          'object_id': {
-            format: value => value,
+          'date_id': {
+            format: value => DateFilterUtils.parseDateString(value).format('MMM DD, YYYY'),
           },
-          'domain_name': {
-            format: value => value,
-          },
-          'referrer': {
-            format: value => value,
+          'count_loads': {
+            format: value => ReportHelper.numberOrZero(value),
           },
           'count_plays': {
-            format: value => value,
+            format: value => parseInt(value, 10),
+          },
+          'sum_time_viewed': {
+            format: value => ReportHelper.numberOrZero(value),
+          },
+          'added_entries': {
+            format: value => ReportHelper.numberOrZero(value),
           },
         }
-      }
-    };
-  }
-  
-  public getGraphConfig(isCompare): EChartOption {
-    return {
-      color: [getPrimaryColor('time'), getSecondaryColor('time')],
-      textStyle: { fontFamily: 'Lato' },
-      grid: {
-        top: 24, left: 10, bottom: 30, right: 10, containLabel: true
       },
-      tooltip: {
-        trigger: 'axis',
-        backgroundColor: '#ffffff',
-        borderColor: '#dadada',
-        borderWidth: 1,
-        extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);',
-        textStyle: { color: '#999999' },
-        axisPointer: { animation: false },
-        formatter: params => {
-          if (params.length > 1) {
-            const { value, direction } = this._trendService.calculateTrend(params[0].data, params[1].data);
-            const icon = this._trendIcon.transform(direction);
-  
-            return `<div class="kGraphTooltip" style="display: flex; align-items: center"><div><span class="kBullet" style="color: ${getPrimaryColor('time')}">&bull;</span>&nbsp;<span style="font-weight: bold; color: #333333">${this._translate.instant('app.user.minutesViewed', [params[0].data])}</span><br><span class="kBullet" style="color: ${getSecondaryColor('time')}">&bull;</span>&nbsp;<span style="font-weight: bold; color: #333333">${this._translate.instant('app.user.minutesViewed', [params[1].data])}</span></div><span class="kTrend" style="margin-left: 8px"><i class="${icon}"></i><span>${value}%</span></span></div>`;
-          }
-
-          return `<div class="kGraphTooltip"><span class="kBullet" style="color: ${getPrimaryColor('time')}">&bull;</span>&nbsp;<span style="font-weight: bold; color: #333333">${this._translate.instant('app.user.minutesViewed', [params[0].data])}</span></div>`;
-        }
+      [ReportDataSection.graph]: {
+        fields: {}
       },
-      xAxis: {
-        type: 'category',
-        data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        axisLine: { show: false },
-        axisTick: { show: false },
-        axisLabel: {
-          fontWeight: 'bold',
-          fontSize: 12,
-          color: '#999999'
-        },
-      },
-      yAxis: {
-        type: 'value',
-        axisLine: { show: false },
-        axisLabel: { show: false },
-        axisTick: { show: false },
-        splitLine: { show: false },
-      },
-      series: [
-        {
-          data: [120, 200, 150, 80, 70, 110, 130],
-          type: 'bar',
-          barMaxWidth: '34px',
-          barMinWidth: '16px',
-          itemStyle: {
-            barBorderRadius: 2
-          },
-          markLine: {
-            animation: false,
-            symbol: false,
-            lineStyle: {
-              color: '#ce5e19',
-              width: 2,
-            },
-            label: {
-              show: true,
-              position: 'middle',
-              formatter: params => this._translate.instant('app.user.dailyAvgViews', [ReportHelper.numberOrZero(params.value)]),
-            },
-            data: [{ type: 'average' }]
-          },
-        },
-        isCompare ? {
-          data: [220, 100, 150, 81, 20, 10, 230],
-          type: 'bar',
-          barMaxWidth: '34px',
-          barMinWidth: '16px',
-          itemStyle: {
-            barBorderRadius: 2
-          }
-        } : null
-      ].filter(Boolean)
     };
   }
 }
