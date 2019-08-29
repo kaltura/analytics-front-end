0bdadf4234d4d09ad68ccf4a782c6ca98ee144ea
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-08-29 09:47:38 +0300

    update compare filter on user drilldown

--- src/app/modules/bandwidth/views/end-user/end-user-storage.component.ts
+++ src/app/modules/bandwidth/views/end-user/end-user-storage.component.ts
@@ -145,8 +145,10 @@ export class EndUserStorageComponent implements OnInit, OnDestroy {
 
       if (this._drillDown) {
         this.filter.userIds = this._drillDown;
+        this.compareFilter.userIds = this._drillDown;
       } else {
         delete this.filter.userIds;
+        delete this.compareFilter.userIds;
       }
 
       this._updateExportConfig();
