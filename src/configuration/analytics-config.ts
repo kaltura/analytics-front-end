b5f1994ff7fa7ad562af4d92b57e4222e1f0ec65
Stas <staskgt@gmail.com>
2019-09-25 14:14:27 +0300

    chore: bump version

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -67,7 +67,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '1.5.0',
+  appVersion: '1.5.1',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
