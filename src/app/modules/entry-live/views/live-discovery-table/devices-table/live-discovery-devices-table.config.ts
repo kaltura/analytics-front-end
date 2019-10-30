0e42afb1ad4e1f822001416dfe5ef05cb1e96c0c
Stas Kohut <diamond-darrell@users.noreply.github.com>
2019-10-30 15:12:03 +0200

    feat(AN-1046): update user buffering calculation (#317)
    * feat(AN-1046): update user buffering calculation
    
    * fix report metric
    
    * fix typo
    
    * fix percents format

--- src/app/modules/entry-live/views/live-discovery-table/devices-table/live-discovery-devices-table.config.ts
+++ src/app/modules/entry-live/views/live-discovery-table/devices-table/live-discovery-devices-table.config.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { GraphType, ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
+import { ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
 import { ReportHelper } from 'shared/services';
 
 @Injectable()
@@ -36,8 +36,8 @@ export class LiveDiscoveryDevicesTableConfig extends ReportDataBaseConfig {
             format: value => ReportHelper.numberOrZero(value),
             sortOrder: 2,
           },
-          'view_unique_buffering_users': {
-            format: value => value,
+          'view_buffer_time_ratio': {
+            format: value => ReportHelper.percents(value),
             sortOrder: 3,
           },
           'sum_view_time': {
@@ -60,8 +60,8 @@ export class LiveDiscoveryDevicesTableConfig extends ReportDataBaseConfig {
             format: value => ReportHelper.numberOrZero(value),
             sortOrder: 2,
           },
-          'view_unique_buffering_users': {
-            format: value => value,
+          'view_buffer_time_ratio': {
+            format: value => ReportHelper.percents(value),
             sortOrder: 4,
           },
           'sum_view_time': {
--- src/app/modules/entry-live/views/live-discovery-table/users-table/live-discovery-users-table.config.ts
+++ src/app/modules/entry-live/views/live-discovery-table/users-table/live-discovery-users-table.config.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { GraphType, ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
+import { ReportDataBaseConfig, ReportDataConfig, ReportDataSection } from 'shared/services/storage-data-base.config';
 import { ReportHelper } from 'shared/services';
 
 @Injectable()
