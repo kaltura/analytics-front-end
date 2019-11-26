e26a186c4bfb720ec00b114fe9dd189a7ce821c5
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-11-26 07:57:04 +0200

    KMS Integration

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -86,11 +86,11 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '1.8.0',
+  appVersion: '1.8.1',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
-  originTarget: window.location.origin,
+  originTarget: '*',
   multiAccount: false,
 };
 
