8fad0d55c1d434d451a339cc4c574fa992e3fc28
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-11-28 14:37:58 +0200

    add IV icon + export totals

--- src/app/modules/playlist/views/path/path-export.config.ts
+++ src/app/modules/playlist/views/path/path-export.config.ts
@@ -14,48 +14,12 @@ export class PathExportConfig extends ExportConfigService {
   public getConfig(viewConfig?: ViewConfig): ExportItem[] {
     const config: ExportItem[] = [
       {
-        id: 'userEngagement',
-        label: this._translate.instant('app.entry.exportLabels.userEngagement'),
-        reportType: reportTypeMap(KalturaReportType.userTopContent),
-        sections: [KalturaReportExportItemType.table],
-        order: '-count_plays',
-      },
-      {
-        id: 'performance',
-        label: this._translate.instant('app.entry.exportLabels.videoPerformance'),
-        reportType: reportTypeMap(KalturaReportType.userTopContent),
-        sections: [KalturaReportExportItemType.graph],
-        order: '-date_id',
-      },
-      {
-        id: 'impressions',
-        label: this._translate.instant('app.entry.exportLabels.impressions'),
-        reportType: reportTypeMap(KalturaReportType.contentDropoff),
+        id: "totals",
+        label: this._translate.instant('app.playlist.exportLabels.highlights'),
+        reportType: reportTypeMap(KalturaReportType.interactiveVideoHighlights),
         sections: [KalturaReportExportItemType.total],
-        order: '-count_plays',
-      },
-      {
-        id: 'geo',
-        label: this._translate.instant('app.entry.exportLabels.topCountries'),
-        reportType: reportTypeMap(KalturaReportType.mapOverlayCountry),
-        sections: [KalturaReportExportItemType.table],
-        order: '-count_plays',
-      },
-      {
-        id: 'devices',
-        label: this._translate.instant('app.entry.exportLabels.devicesOverview'),
-        reportType: reportTypeMap(KalturaReportType.platforms),
-        sections: [KalturaReportExportItemType.table],
-      },
-      {
-        id: 'syndication',
-        label: this._translate.instant('app.entry.exportLabels.syndication'),
-        reportType: reportTypeMap(KalturaReportType.topSyndication),
-        sections: [KalturaReportExportItemType.table],
-        order: '-count_plays',
-      },
+      }
     ];
-  
     return viewConfig ? config.filter((item: ExportItem) => viewConfig[item.id]) : config;
   }
 
