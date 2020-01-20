0bfbdbf77cca6e4d0257174010b07dda88f65d45
amir.chervinsky <amir.chervinsky@kaltura.com>
2020-01-20 08:33:45 +0200

    bump Analytics v1.12.0

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -91,7 +91,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '1.11.1',
+  appVersion: '1.12.0',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
