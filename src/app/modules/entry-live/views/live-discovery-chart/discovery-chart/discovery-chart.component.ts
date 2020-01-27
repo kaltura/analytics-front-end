d7b30ff6565bd7f6db547176f88da7ddd9b821b4
amir.chervinsky <amir.chervinsky@kaltura.com>
2020-01-27 10:52:09 +0200

    do not normalize Avg. latency rate

--- src/app/modules/entry-live/views/live-discovery-chart/discovery-chart/discovery-chart.component.ts
+++ src/app/modules/entry-live/views/live-discovery-chart/discovery-chart/discovery-chart.component.ts
@@ -247,7 +247,7 @@ export class DiscoveryChartComponent implements OnDestroy{
   }
 
   private shouldNormalize(metric: string): boolean {
-    const normalizedMetrics = ['view_unique_audience', 'viewers', 'view_unique_audience_dvr', 'viewers_dvr', 'view_unique_engaged_users', 'viewers_engagement', 'avg_view_live_latency'];
+    const normalizedMetrics = ['view_unique_audience', 'viewers', 'view_unique_audience_dvr', 'viewers_dvr', 'view_unique_engaged_users', 'viewers_engagement'];
     return normalizedMetrics.indexOf(metric) > -1;
   }
 
