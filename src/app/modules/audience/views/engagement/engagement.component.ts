fb818f90143bf6a0eb4359ecedaec373ee742d7d
amirch1 <amir.chervinsky@kaltura.com>
2020-08-09 15:47:50 +0300

    export PDF example for engagement screen. TODO: add html2pdf using npm instead of script, layout, KMS support, add page breaks, generic usage across screens (apx. 2 days). ref: https://ekoopmans.github.io/html2pdf.js/

--- src/app/modules/audience/views/engagement/engagement.component.ts
+++ src/app/modules/audience/views/engagement/engagement.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 import { DateChangeEvent, DateRanges } from 'shared/components/date-filter/date-filter.service';
 import { KalturaEndUserReportInputFilter, KalturaReportInterval, KalturaReportType } from 'kaltura-ngx-client';
 import { KalturaLogger } from '@kaltura-ng/kaltura-logger';
@@ -40,20 +40,20 @@ export class EngagementComponent {
       searchInAdminTags: false
     }
   );
-  
+
   constructor(private _exportConfigService: EngagementExportConfig) {
     this._exportConfig = _exportConfigService.getConfig();
   }
-  
-  
+
+
   public _onDateFilterChange(event: DateChangeEvent): void {
     this._dateFilter = event;
   }
-  
+
   public _onRefineFilterChange(event: RefineFilter): void {
     this._refineFilter = event;
   }
-  
+
   public _onDrillDown(event: string): void {
     let update: Partial<ExportItem> = {};
     if (event) {
@@ -61,5 +61,32 @@ export class EngagementComponent {
     }
 
     this._exportConfig = EngagementExportConfig.updateConfig(this._exportConfigService.getConfig(), 'syndication', update);
+  }
+
+  public exportPDF(el: any): void{
+    el.style.display = 'none';
+    var element = document.getElementById('reportToExport');
+    element.style.paddingLeft = 20 + 'px';
+    element.style.backgroundColor = '#f2f2f2';
+    let scrollHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+    var opt = {
+      margin:       0,
+      filename:     'myfile.pdf',
+      image:        { type: 'jpeg', quality: 0.95 },
+      html2canvas:  { width: document.getElementsByClassName('kContent')[0].clientWidth + 40, height: scrollHeight + window.innerHeight -50, useCORS: true, backgroundColor: '#f2f2f2' },
+      jsPDF:        { units: 'px', orientation: 'portrait' }
+    };
+    window['html2pdf'](element, opt);
+    setTimeout(() =>{
+      element.style.paddingLeft = null;
+      element.style.backgroundColor = null;
+      el.style.display = 'block';
+    },0);
+
+
   }
 }
