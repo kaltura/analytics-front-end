f4ec0ce69c12107ba0dfd9b1b15b1fdcdee8fe64
amirch1 <amir.chervinsky@kaltura.com>
2021-01-17 14:49:55 +0200

    bump Analytics version to v2.2.4

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -92,7 +92,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '2.2.3',
+  appVersion: '2.2.4',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
