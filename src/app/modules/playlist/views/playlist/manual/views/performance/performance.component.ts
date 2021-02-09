58cc75168bafb51a2ddcc516afb4c2331a769003
amirch1 <amir.chervinsky@kaltura.com>
2021-02-09 16:23:29 +0200

    playlist - update totals on each date change

--- src/app/modules/playlist/views/playlist/manual/views/performance/performance.component.ts
+++ src/app/modules/playlist/views/playlist/manual/views/performance/performance.component.ts
@@ -177,7 +177,7 @@ export class ManualPlaylistPerformanceComponent extends ManualPlaylistBase imple
           }
           this.highlights$.next({ current: report, compare: compare, busy: false, error: null });
 
-          if (report.totals && (!this._tabsData.length || this._filter.userIds)) {
+          if (report.totals) {
             this._handleTotals(report.totals); // handle totals
           }
 
