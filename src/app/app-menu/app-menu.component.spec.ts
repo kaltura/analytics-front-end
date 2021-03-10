b46993858dd820f4b668556d1b4397e9085acb2f
amirch1 <amir.chervinsky@kaltura.com>
2021-03-10 08:37:43 +0200

    update to Angular 11

--- src/app/app-menu/app-menu.component.spec.ts
+++ src/app/app-menu/app-menu.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppMenuComponent } from './app-menu.component';
 
@@ -6,7 +6,7 @@ describe('AppMenuComponent', () => {
   let component: AppMenuComponent;
   let fixture: ComponentFixture<AppMenuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AppMenuComponent ]
     })
