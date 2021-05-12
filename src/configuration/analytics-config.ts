8769ce53b619461aa4e1aa4070c4126a36146df8
amirch1 <amir.chervinsky@kaltura.com>
2021-05-12 17:20:34 +0300

    patch: remove user drill-down link in Entry user engagement

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -91,7 +91,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '1.16.2',
+  appVersion: '1.16.3',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
