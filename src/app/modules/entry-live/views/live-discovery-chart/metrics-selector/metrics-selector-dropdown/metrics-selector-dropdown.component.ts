e0532c272120be30e068545d2fa3ccd2b876546a
Viktor Kyryltsev <85935219+viktor-kyryltsev-kaltura@users.noreply.github.com>
2021-08-22 10:12:57 +0300

    feat(live-entry): change discovery metrics selector and add additional metrics AN-22528 (#440)
    * feat(entry-live): change metrics selector to two columns
    
    * fixed popup
    
    * revert index.html
    
    * feat: add new metric selector popup
    
    * feat: add metrics to config
    
    Co-authored-by: amirch1 <amir.chervinsky@kaltura.com>

--- src/app/modules/entry-live/views/live-discovery-chart/metrics-selector/metrics-selector-dropdown/metrics-selector-dropdown.component.ts
+++ src/app/modules/entry-live/views/live-discovery-chart/metrics-selector/metrics-selector-dropdown/metrics-selector-dropdown.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { SelectItem } from 'primeng/api';
-import { getPrimaryColor } from 'shared/utils/colors';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {SelectItem, SelectItemGroup} from 'primeng/api';
+import {getPrimaryColor} from 'shared/utils/colors';
+import {PopupWidgetComponent} from "@kaltura-ng/kaltura-ui";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-metrics-selector-dropdown',
@@ -8,17 +10,54 @@ import { getPrimaryColor } from 'shared/utils/colors';
   styleUrls: ['./metrics-selector-dropdown.component.scss']
 })
 export class MetricsSelectorDropdownComponent {
-  @Input() options: SelectItem[] = [];
+  @ViewChild(PopupWidgetComponent) _popup: PopupWidgetComponent;
+  @Input() options: SelectItemGroup[] = [];
   @Input() selection: string;
   @Input() colorsMap: { [metric: string]: string } = {};
-  
+
   @Output() selectionChange = new EventEmitter<string>();
   @Output() change = new EventEmitter<string>();
-  
-  public _defaultColor = getPrimaryColor();
-  
+  public _popupOpened = false;
+
+  public _defaultColor = '#fff';
+
+  public noneOption = {
+    label: this._translate.instant(`app.entryLive.discovery.none`),
+    value: 'none'
+  };
+
+  constructor(private _translate: TranslateService) {
+  }
+
+  public getSelectedLabel() {
+    let option: string;
+    this.options.forEach(optionGroup => {
+      const foundOption = optionGroup.items.find(item => item.value === this.selection);
+      if (foundOption) {
+        option = foundOption.label;
+      }
+    });
+    return option || this.noneOption.label;
+  }
+
   public _onChange(): void {
     this.selectionChange.emit(this.selection);
     this.change.emit(this.selection);
+  }
+
+  _onPopupOpen() {
+    this._popupOpened = true;
+  }
+
+  _onPopupClose() {
+    this._popupOpened = false;
+  }
+
+  selectMetric(metric: string) {
+    this.selection = metric;
+    this._onChange();
+    if (this._popup) {
+      this._popup.close();
+    }
   }
 }
--- src/app/modules/entry-live/views/live-discovery-chart/metrics-selector/metrics-selector.component.ts
+++ src/app/modules/entry-live/views/live-discovery-chart/metrics-selector/metrics-selector.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
-import { SelectItem } from 'primeng/api';
+import { SelectItem, SelectItemGroup } from 'primeng/api';
 import { TranslateService } from '@ngx-translate/core';
 import { ReportDataFields } from 'shared/services/storage-data-base.config';
 
@@ -16,44 +16,53 @@ export interface MetricsSelectorChangeEvent {
 export class MetricsSelectorComponent implements OnChanges {
   @Input() fields: ReportDataFields;
   @Input() colorsMap: { [metric: string]: string } = {};
-  
+
   @Output() selectorChange = new EventEmitter<MetricsSelectorChangeEvent>();
-  
+
   private _metrics: string[] = [];
-  
-  public _mainMetricsOptions: SelectItem[] = [];
-  public _secondaryMetricsOptions: SelectItem[] = [];
+
+  public _mainMetricsOptions: SelectItemGroup[] = [];
+  public _secondaryMetricsOptions: SelectItemGroup[] = [];
   public _selectedMain: string;
   public _selectedSecondary: string;
-  
+
   constructor(private _translate: TranslateService) {
   }
-  
+
   ngOnChanges() {
     if (this.fields) {
       this._metrics = Object.keys(this.fields);
       this._onChange(true);
     }
   }
-  
+
   private _updateOptions(initial: boolean): void {
     this._selectedMain = initial ? this._metrics[0] : this._selectedMain;
-    
-    this._secondaryMetricsOptions = this._getOptions(this._metrics.filter(metric => metric !== this._selectedMain));
-    this._selectedSecondary = initial ? this._secondaryMetricsOptions[0].value : this._selectedSecondary;
-    
-    this._mainMetricsOptions = this._getOptions(this._metrics.filter(metric => metric !== this._selectedSecondary));
-    this._mainMetricsOptions.unshift(...this._getOptions(['none']));
-    this._secondaryMetricsOptions.unshift(...this._getOptions(['none']));
+
+    this._secondaryMetricsOptions = this._getOptionGroups(this._selectedMain);
+    this._selectedSecondary = initial ? this._secondaryMetricsOptions[0].items[0].value : this._selectedSecondary;
+
+    this._mainMetricsOptions = this._getOptionGroups(this._selectedSecondary);
   }
-  
-  private _getOptions(metrics: string[]): SelectItem[] {
-    return metrics.map(metric => ({
+
+  private _getOptionGroups(selectedOption: string) {
+    return [{
+      label: this._translate.instant('app.entryWebcast.engagement.title'),
+      items: this._getOptions(this._metrics.filter(metric => metric !== selectedOption), 'engagement')
+    }, {
+      label: this._translate.instant('app.entryWebcast.quality.title'),
+      items: this._getOptions(this._metrics.filter(metric => metric !== selectedOption), 'qualityOfService')
+    }];
+  }
+
+  private _getOptions(metrics: string[], group: string) {
+    return metrics.filter(metric => this.fields[metric].group === group).map(metric => ({
       label: this._translate.instant(`app.entryLive.discovery.${metric}`),
       value: metric,
+      items: []
     }));
   }
-  
+
   public _onChange(initial = false): void {
     this._updateOptions(initial);
 
