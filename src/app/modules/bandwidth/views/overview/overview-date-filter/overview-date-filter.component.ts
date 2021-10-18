bceabc2b3b5b6385c94511f5a285d652cc867ac7
amirch1 <amir.chervinsky@kaltura.com>
2021-10-18 14:37:34 +0300

    date selector

--- src/app/modules/bandwidth/views/overview/overview-date-filter/overview-date-filter.component.ts
+++ src/app/modules/bandwidth/views/overview/overview-date-filter/overview-date-filter.component.ts
@@ -1,11 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {OverviewDateRange} from "./overview-date-range.type";
 import {DateFilterUtils} from "shared/components/date-filter/date-filter-utils";
-import {analyticsConfig} from "configuration/analytics-config";
 import {TranslateService} from "@ngx-translate/core";
 import {KalturaReportInterval} from "kaltura-ngx-client";
 import {AuthService} from "shared/services";
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-overview-date-filter',
@@ -14,16 +12,13 @@ import * as moment from 'moment';
 })
 export class OverviewDateFilterComponent implements OnInit {
 
-  @Input() availableDateRange: AvailabilityDateRange;
   @Output() filterChange = new EventEmitter<OverviewDateRange>();
 
-  public selectedDateRange: OverviewDateRange;
-  public appliedDateRange: OverviewDateRange;
+  public selectedDateRangeKey = 'month-0';
+  public selectedDateRange: OverviewDateRange = null;
+  public labelPostfix = '';
   public monthlyDateRangeItems: OverviewDateRange[] = [];
   public yearlyDateRangeItems: OverviewDateRange[] = [];
-
-  public applyDisabled = false;
-  private localeData = DateFilterUtils.getLocalData(analyticsConfig.locale);
 
   constructor(private _translate: TranslateService,
               private _authService: AuthService) {
@@ -31,18 +26,11 @@ export class OverviewDateFilterComponent implements OnInit {
 
   ngOnInit(): void {
     this._createTimeRanges();
-    /*
-    const currentMonth = this._translate.instant('app.dateFilter.prefix.month');
-    const currentYear = this._translate.instant('app.dateFilter.prefix.year');
-    const specific = this._translate.instant('app.dateFilter.specificLabel');
-    this.monthlyDateRangeItems = getMonths(this.availableDateRange, this.localeData.monthNames, currentMonth, specific).reverse();
-    this.yearlyDateRangeItems = getYears(this.availableDateRange, currentYear, specific).reverse();
     if (!this.selectedDateRange) {
-      this.selectedDateRange = this.monthlyDateRangeItems[0];
       setTimeout(() => {
         this.updateDataRanges();
       });
-    }*/
+    }
   }
 
   private _createTimeRanges(): void {
@@ -51,27 +39,33 @@ export class OverviewDateFilterComponent implements OnInit {
     const partnerCreationYear = partnerCreationDate.getFullYear();
     const currentYear = today.getFullYear();
     this.yearlyDateRangeItems = [{
+      key: 'year-0',
       label: this._translate.instant('app.dateFilter.currentYear'),
       startDate: DateFilterUtils.toServerDate(new Date(`${currentYear}-01-01`), true),
-      endDate: DateFilterUtils.toServerDate(today, false)
+      endDate: DateFilterUtils.toServerDate(today, false),
+      interval: KalturaReportInterval.months
     }];
     // add up to 5 years to available years
     for (let i = 1; i < 5; i++) {
       const year = currentYear - i;
       if (year >= partnerCreationYear) {
         this.yearlyDateRangeItems.push({
+          key: `year-${i}`,
           label: year.toString(),
           startDate: DateFilterUtils.toServerDate(new Date(`${year}-01-01`), true),
-          endDate: DateFilterUtils.toServerDate(new Date(`${year}-12-31`), false)
+          endDate: DateFilterUtils.toServerDate(new Date(`${year}-12-31`), false),
+          interval: KalturaReportInterval.months
         });
       }
     }
     let currentMonth = today.getMonth() + 1;
     const monthsDiff = today.getMonth() - partnerCreationDate.getMonth() + (12 * (today.getFullYear() - partnerCreationDate.getFullYear()));
     this.monthlyDateRangeItems = [{
+      key: 'month-0',
       label: this._translate.instant('app.dateFilter.currentMonth'),
       startDate: DateFilterUtils.toServerDate(new Date(`${currentYear}-${currentMonth}-01`), true),
-      endDate: DateFilterUtils.toServerDate(new Date(currentYear,currentMonth,0), false)
+      endDate: DateFilterUtils.toServerDate(new Date(currentYear,currentMonth,0), false),
+      interval: KalturaReportInterval.days
     }];
     // add up to 12 months to available months
     let year = currentYear;
@@ -84,58 +78,28 @@ export class OverviewDateFilterComponent implements OnInit {
         }
         const monthName = new Date(`${year}-${month}-01`).toLocaleString('default', { month: 'long' });
         this.monthlyDateRangeItems.push({
+          key: `month-${i}`,
           label: `${monthName} ${year}`,
           startDate: DateFilterUtils.toServerDate(new Date(`${year}-${month}-01`), true),
-          endDate: DateFilterUtils.toServerDate(new Date(year,month,0), false)
+          endDate: DateFilterUtils.toServerDate(new Date(year,month,0), false),
+          interval: KalturaReportInterval.days
         });
       }
     }
   }
 
   updateDataRanges() {
-    this.appliedDateRange = this.selectedDateRange;
+    const keys = this.selectedDateRangeKey.split('-');
+    if (keys && keys.length === 2) {
+      const index = parseInt(keys[1]);
+      if (keys[0] === "year") {
+        this.selectedDateRange = this.yearlyDateRangeItems[index];
+        this.labelPostfix = index === 0 ? new Date().getFullYear().toString() : '';
+      } else {
+        this.selectedDateRange = this.monthlyDateRangeItems[index];
+        this.labelPostfix = index === 0 ? new Date().toLocaleString('default', { month: 'long' }) + ' ' + new Date().getFullYear().toString() : '';
+      }
+    }
     this.filterChange.emit(this.selectedDateRange);
   }
 }
-
-export interface AvailabilityDateRange {
-  startDate: Date;
-  endDate: Date;
-}
-/*
-function getMonths(dateRange: AvailabilityDateRange, monthNames: string[], currentMonthLabel: string, specificLabel: string) {
-  let startMonth = dateRange.startDate.getMonth();
-  let startYear = dateRange.startDate.getFullYear();
-  let endMonth = dateRange.endDate.getMonth();
-  let endYear = dateRange.endDate.getFullYear();
-  let dates: OverviewDateRange[] = [];
-  const currentDate = new Date();
-
-  for (let i = startYear; i <= endYear; i++) {
-    let endMon = i !== endYear ? 11 : endMonth;
-    let startMon = i === startYear ? startMonth : 0;
-    for (let month = startMon; month <= endMon; month = month > 12 ? month % 12 || 11 : month + 1) {
-      const label = monthNames[month] + ' ' + i;
-      const isCurrent = currentDate.getFullYear() && currentDate.getMonth() === month;
-      const prefix = isCurrent ? currentMonthLabel : specificLabel;
-      dates.push({label, prefix, isCurrent, value: new Date(i, month), interval: KalturaReportInterval.months});
-    }
-  }
-  return dates;
-}
-
-function getYears(dateRange: AvailabilityDateRange, currentYearLabel: string, specificLabel: string) {
-  let startYear = dateRange.startDate.getFullYear();
-  let endYear = dateRange.endDate.getFullYear();
-  let dates: OverviewDateRange[] = [];
-  const currentDate = new Date();
-
-  for (let i = startYear; i <= endYear; i++) {
-    const label = i.toString();
-    const isCurrent = i === currentDate.getFullYear();
-    const prefix = isCurrent ? currentYearLabel : specificLabel;
-    dates.push({label, prefix, isCurrent, value: new Date(i, 0), interval: KalturaReportInterval.months}); // TODO: needs to have years interval
-  }
-  return dates;
-}
-*/
--- src/app/modules/bandwidth/views/overview/overview-date-filter/overview-date-range.type.ts
+++ src/app/modules/bandwidth/views/overview/overview-date-filter/overview-date-range.type.ts
@@ -1,5 +1,9 @@
+import {KalturaReportInterval} from "kaltura-ngx-client";
+
 export interface OverviewDateRange {
   label: string;
   startDate: number;
   endDate: number;
+  key: string;
+  interval: KalturaReportInterval;
 }
