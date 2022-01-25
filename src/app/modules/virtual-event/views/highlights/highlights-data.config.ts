01d9d952b8eaa0d5bc0359efb7b70fb2160910eb
amirch1 <amir.chervinsky@kaltura.com>
2022-01-25 14:46:07 +0200

    ve table from graph

--- src/app/modules/virtual-event/views/highlights/highlights-data.config.ts
+++ src/app/modules/virtual-event/views/highlights/highlights-data.config.ts
@@ -3,6 +3,8 @@ import { ReportDataConfig, ReportDataSection, ReportDataBaseConfig } from 'share
 import {TranslateService} from "@ngx-translate/core";
 import {ReportHelper} from "shared/services";
 import {getPrimaryColor, getSecondaryColor} from "shared/utils/colors";
+import {DateFilterUtils} from "shared/components/date-filter/date-filter-utils";
+import {analyticsConfig} from "configuration/analytics-config";
 
 @Injectable()
 export class HighlightsDataConfig extends ReportDataBaseConfig {
@@ -51,6 +53,30 @@ export class HighlightsDataConfig extends ReportDataBaseConfig {
           'unregistered': {
             format: value => ReportHelper.numberOrNA(value),
             title: this._translate.instant(`app.ve.unregistered`),
+          }
+        }
+      },
+      [ReportDataSection.table]: {
+        fields: {
+          'date_id': {
+            format: value => DateFilterUtils.formatFullDateString(value),
+            nonComparable: true,
+          },
+          'registered': {
+            format: value => ReportHelper.numberOrZero(value, true),
+            sortOrder: 1
+          },
+          'confirmed': {
+            format: value => ReportHelper.numberOrZero(value, true),
+            sortOrder: 2
+          },
+          'participated': {
+            format: value => ReportHelper.numberOrZero(value, true),
+            sortOrder: 3
+          },
+          'unregistered': {
+            format: value => ReportHelper.numberOrZero(value, true),
+            sortOrder: 4
           }
         }
       }
