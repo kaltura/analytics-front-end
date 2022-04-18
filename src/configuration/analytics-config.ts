37dea5ec50089d5fb8ed7264805b529a03bd4747
amirch1 <amir.chervinsky@kaltura.com>
2022-04-18 08:12:32 +0300

    deploy and load players for Analytics + bump version to v2.9.3

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -96,7 +96,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '2.9.2',
+  appVersion: '2.9.3',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
