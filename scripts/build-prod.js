66f5ee8f8392d77dfd0c3432841b07e86f45e674
amirch1 <amir.chervinsky@kaltura.com>
2023-06-28 10:02:20 +0300

    chore: updated build script

--- scripts/build-prod.js
+++ scripts/build-prod.js
@@ -21,6 +21,7 @@ catch (error) {
 }
 
 // STEP: check existence of tag with the version
+/*
 try {
   const tagExistence = execSync(`git tag -l "v${versionNumber}"`).toString();
   if (tagExistence) {
@@ -31,9 +32,12 @@ try {
   console.error(error);
   process.exit(1);
 }
-
+*/
 const packageRoot = findRoot(process.cwd());
 if (!packageRoot) throw new Error("couldn't find package root");
+
+// STEP: empty dist folder
+fse.emptyDirSync(path.resolve(packageRoot, 'dist'));
 
 // STEP: update src/configuration/analytics-config.ts
 const configFileName = "src/configuration/analytics-config.ts";
@@ -45,23 +49,45 @@ configData = configData.replace(regex, `appVersion: '${versionNumber}'`);
 fs.writeFileSync(configFilePath, configData, 'utf-8');
 console.log('analytics-config.ts has been updated!');
 
+// STEP: update deploy/config.ini
+const v2PlayerConfigFileName = "deploy/config.ini";
+const v2ConfigFilePath = path.resolve(packageRoot, v2PlayerConfigFileName);
+let v2ConfigData = fs.readFileSync(v2ConfigFilePath, 'utf8');
+const regex_v2 = /component.version=v\d\.\d\.\d/;
+v2ConfigData = v2ConfigData.replace(regex_v2, `component.version=v${versionNumber}`);
+
+fs.writeFileSync(v2ConfigFilePath, v2ConfigData, 'utf-8');
+console.log('v2 player config has been updated!');
+
+// STEP: update deploy_v7/config.ini
+const v7PlayerConfigFileName = "deploy_v7/config.ini";
+const v7ConfigFilePath = path.resolve(packageRoot, v7PlayerConfigFileName);
+let v7ConfigData = fs.readFileSync(v7ConfigFilePath, 'utf8');
+const regex_v7 = /component.version=v\d\.\d\.\d/;
+v7ConfigData = v7ConfigData.replace(regex_v7, `component.version=v${versionNumber}`);
+
+fs.writeFileSync(v7ConfigFilePath, v7ConfigData, 'utf-8');
+console.log('v7 player config has been updated!');
+
 // STEP: update package.json
 const packageJsonPath = path.resolve(packageRoot, 'package.json');
 const packageData = fs.readFileSync(packageJsonPath);
 let packageJsonObj = JSON.parse(packageData);
+const commitRequired = packageJsonObj.version !== versionNumber;
 packageJsonObj.version = versionNumber;
 packageJsonObj = JSON.stringify(packageJsonObj, null, 2);
 
 fs.writeFileSync(packageJsonPath, packageJsonObj);
 console.log('Package.json has been updated!');
 
-// STEP: commit changed files to git
-try {
-  execSync(`git commit -am "bump analytics version to v${versionNumber}"`);
-}
-catch (error) {
-  console.error('Git commit operation failed. Verify you are logged into Github and have permissions to commit to this repository');
-  process.exit(1);
+// STEP: commit changed files to git if needed
+if (commitRequired) {
+  try {
+    execSync(`git commit -am "bump analytics version to v${versionNumber}"`);
+  } catch (error) {
+    console.error('Git commit operation failed. Verify you are logged into Github and have permissions to commit to this repository');
+    process.exit(1);
+  }
 }
 
 // STEP: push commit to origin
@@ -99,6 +125,16 @@ try {
   process.exit(1);
 }
 
+// STEP: copy player deployment folders
+console.log('---copy player deployment folders---');
+try {
+  fse.copySync(path.resolve(packageRoot, 'deploy'), dir + '/deploy');
+  fse.copySync(path.resolve(packageRoot, 'deploy_v7'), dir + '/deploy_v7');
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
+
 let opts = {
   "cwd": `dist`,
   "env": process.env
@@ -113,8 +149,12 @@ catch (error) {
   process.exit(1);
 }
 
+// STEP: delete dist folders
+fse.remove(dir);
+fse.remove(path.resolve(packageRoot, 'dist/analytics-front-end'));
 
 // STEP: create tag with new version
+/*
 try {
   execSync(`git tag v${versionNumber}`);
 }
@@ -142,5 +182,7 @@ catch (error) {
   console.error('cannot upload release: ', error);
   process.exit(1);
 }
-
 console.log('\x1b[32m%s\x1b[0m', `Version created successfully! You can find the new release here: https://github.com/kaltura/analytics-front-end/releases/tag/v${versionNumber}`);
+*/
+console.log('\x1b[32m%s\x1b[0m', `Version created successfully! You can find the zipped version here: dist/${zipName}`);
+
