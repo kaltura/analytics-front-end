a38f20dbc212b9b41b1b5e865d95ba26630fb66d
amirch1 <amir.chervinsky@kaltura.com>
2023-07-16 12:33:52 +0300

    hackathon initial files

--- src/app/modules/welcome/views/search-terms/search-terms.component.ts
+++ src/app/modules/welcome/views/search-terms/search-terms.component.ts
@@ -10,6 +10,7 @@ import {
 import { analyticsConfig } from 'configuration/analytics-config';
 import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
 import { ErrorsManagerService } from 'shared/services';
+import { cancelOnDestroy } from '@kaltura-ng/kaltura-common';
 import { Configuration, OpenAIApi } from 'openai';
 
 export type Term = {
@@ -25,6 +26,8 @@ export type Term = {
 
 export class SearchTermsComponent implements OnInit, OnDestroy {
   public _loading = false;
+  public _searchTermsLoaded = false;
+  public _aiLoading = false;
   public _blockerMessage: AreaBlockerMessage = null;
   public searchTerms: Term[] = [];
   public AIResponse = 'Loading...';
@@ -41,7 +44,7 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
   }
 
   private loadSearchTerms(): void {
-    // this._loading = true;
+    this._loading = true;
     this._blockerMessage = null;
 
     const today = new Date().getTime();
@@ -59,16 +62,16 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
       })
     });
 
-    this.searchTerms = JSON.parse(`[{"x":"workday","value":403},{"x":"kaltura","value":376},{"x":"caption asr complete","value":240},{"x":"generative ai","value":210},{"x":"powtoon","value":202},{"x":"japan tech lunch session","value":145},{"x":"sesion tq:","value":123},{"x":"song","value":121},{"x":"one accenture","value":118},{"x":"self reflection","value":118},{"x":"hackerland","value":117},{"x":"gen ai","value":115},{"x":"pride","value":112},{"x":"salesforce accenture","value":111},{"x":"sap","value":104},{"x":"power bi","value":102},{"x":"tq","value":101},{"x":"synops","value":100},{"x":"townhall","value":100},{"x":"talent discussion","value":99},{"x":"abcd","value":88},{"x":"team engagement survey","value":80},{"x":"rodolfo eschenbach","value":78},{"x":"total enterprise reinvention","value":76},{"x":"cdms","value":72},{"x":"sustainability","value":65},{"x":"hacker land","value":64},{"x":"summer event","value":64},{"x":"self-reflection","value":63},{"x":"ai","value":59},{"x":"ace","value":58},{"x":"abcd reflection","value":56},{"x":"model minds","value":56},{"x":"accenture song","value":55},{"x":"rpa","value":55},{"x":"priorities","value":54},{"x":"security","value":54},{"x":"flavia picolo","value":53},{"x":"julie sweet","value":53},{"x":"performance achievement","value":53},{"x":"abap","value":52},{"x":"disability","value":51},{"x":"music","value":51},{"x":"genai","value":50},{"x":"mms","value":50},{"x":"finance in five","value":49},{"x":"quantum computing","value":46},{"x":"newsletter","value":45},{"x":"vr","value":45},{"x":"art retention","value":44},{"x":"controllership","value":43},{"x":"h&m","value":43},{"x":"cybersecurity","value":42},{"x":"excel","value":42},{"x":"salesforce","value":42},{"x":"kaltura capture","value":41},{"x":"leadership essentials","value":41},{"x":"audm","value":40},{"x":"sap mdg","value":40},{"x":"commerce","value":39},{"x":"metaverse","value":39},{"x":"mme","value":39},{"x":"myscheduling","value":39},{"x":"storytelling","value":38},{"x":"sap btp","value":37},{"x":"roro","value":36},{"x":"servicenow","value":36},{"x":"cloud first","value":35},{"x":"workday priorities","value":35},{"x":"\\"fy23\\"","value":34},{"x":"chatgpt","value":34},{"x":"#spice","value":33},{"x":"industry x","value":33},{"x":"journey","value":33},{"x":"learning to learn","value":33},{"x":"reverse","value":33},{"x":"bpo","value":32},{"x":"chat gpt","value":32},{"x":"devopsweek2023","value":32},{"x":"downstream","value":32},{"x":"tax","value":32},{"x":"e-mail","value":31},{"x":"teaser","value":31},{"x":"password reset","value":30},{"x":"people","value":30},{"x":"skills","value":30},{"x":"bcm","value":29},{"x":"databricks","value":29},{"x":"ix for","value":28},{"x":"mobilization","value":28},{"x":"code of business ethics","value":27},{"x":"health","value":27},{"x":"solution tablet","value":27},{"x":"town hall","value":27},{"x":"ux design","value":27},{"x":"celonis","value":26},{"x":"devops","value":26},{"x":"let there be change","value":26},{"x":"sap obp","value":26},{"x":"atc nantes","value":24}]`);
-    const chart = window['anychart'].tagCloud();
-    chart.angles([0]);
-    chart.data(this.searchTerms, {mode: "spiral"});
-    chart.container("wordsCloud");
-    chart.draw();
+    // this.searchTerms = JSON.parse(`[{"x":"workday","value":403},{"x":"kaltura","value":376},{"x":"caption asr complete","value":240},{"x":"generative ai","value":210},{"x":"powtoon","value":202},{"x":"japan tech lunch session","value":145},{"x":"sesion tq:","value":123},{"x":"song","value":121},{"x":"one accenture","value":118},{"x":"self reflection","value":118},{"x":"hackerland","value":117},{"x":"gen ai","value":115},{"x":"pride","value":112},{"x":"salesforce accenture","value":111},{"x":"sap","value":104},{"x":"power bi","value":102},{"x":"tq","value":101},{"x":"synops","value":100},{"x":"townhall","value":100},{"x":"talent discussion","value":99},{"x":"abcd","value":88},{"x":"team engagement survey","value":80},{"x":"rodolfo eschenbach","value":78},{"x":"total enterprise reinvention","value":76},{"x":"cdms","value":72},{"x":"sustainability","value":65},{"x":"hacker land","value":64},{"x":"summer event","value":64},{"x":"self-reflection","value":63},{"x":"ai","value":59},{"x":"ace","value":58},{"x":"abcd reflection","value":56},{"x":"model minds","value":56},{"x":"accenture song","value":55},{"x":"rpa","value":55},{"x":"priorities","value":54},{"x":"security","value":54},{"x":"flavia picolo","value":53},{"x":"julie sweet","value":53},{"x":"performance achievement","value":53},{"x":"abap","value":52},{"x":"disability","value":51},{"x":"music","value":51},{"x":"genai","value":50},{"x":"mms","value":50},{"x":"finance in five","value":49},{"x":"quantum computing","value":46},{"x":"newsletter","value":45},{"x":"vr","value":45},{"x":"art retention","value":44},{"x":"controllership","value":43},{"x":"h&m","value":43},{"x":"cybersecurity","value":42},{"x":"excel","value":42},{"x":"salesforce","value":42},{"x":"kaltura capture","value":41},{"x":"leadership essentials","value":41},{"x":"audm","value":40},{"x":"sap mdg","value":40},{"x":"commerce","value":39},{"x":"metaverse","value":39},{"x":"mme","value":39},{"x":"myscheduling","value":39},{"x":"storytelling","value":38},{"x":"sap btp","value":37},{"x":"roro","value":36},{"x":"servicenow","value":36},{"x":"cloud first","value":35},{"x":"workday priorities","value":35},{"x":"\\"fy23\\"","value":34},{"x":"chatgpt","value":34},{"x":"#spice","value":33},{"x":"industry x","value":33},{"x":"journey","value":33},{"x":"learning to learn","value":33},{"x":"reverse","value":33},{"x":"bpo","value":32},{"x":"chat gpt","value":32},{"x":"devopsweek2023","value":32},{"x":"downstream","value":32},{"x":"tax","value":32},{"x":"e-mail","value":31},{"x":"teaser","value":31},{"x":"password reset","value":30},{"x":"people","value":30},{"x":"skills","value":30},{"x":"bcm","value":29},{"x":"databricks","value":29},{"x":"ix for","value":28},{"x":"mobilization","value":28},{"x":"code of business ethics","value":27},{"x":"health","value":27},{"x":"solution tablet","value":27},{"x":"town hall","value":27},{"x":"ux design","value":27},{"x":"celonis","value":26},{"x":"devops","value":26},{"x":"let there be change","value":26},{"x":"sap obp","value":26},{"x":"atc nantes","value":24}]`);
+    // const chart = window['anychart'].tagCloud();
+    // chart.angles([0]);
+    // chart.data(this.searchTerms, {mode: "spiral"});
+    // chart.container("wordsCloud");
+    // chart.draw();
 
     this.loadAiAnalysis();
 
-/*
+
     this._kalturaClient
       .request(request)
       .pipe(
@@ -86,7 +89,7 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
             chart.data(this.searchTerms, {mode: "spiral"});
             chart.container("wordsCloud");
             chart.draw();
-            console.log(JSON.stringify(this.searchTerms));
+            this._searchTermsLoaded = true;
           }
         },
         error => {
@@ -100,11 +103,12 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
             }
           };
           this._blockerMessage = this._errorsManager.getErrorMessage(error, actions);
-        });*/
+        });
 
   }
 
   private loadAiAnalysis(): void {
+    this._aiLoading = true;
     const configuration = new Configuration({
         apiKey: analyticsConfig.customData.openAiKey,
       });
@@ -116,8 +120,11 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
       max_tokens: 256
     }).then(response => {
       this.AIResponse = response?.data?.choices[0]?.text || '';
+      this._aiLoading = false;
     }).catch(error => {
       console.error(error?.response?.data?.error?.message || error);
+      this.AIResponse = 'Analysis not available';
+      this._aiLoading = false;
     });
 
   }
