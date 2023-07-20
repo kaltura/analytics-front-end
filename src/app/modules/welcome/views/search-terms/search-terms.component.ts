2469fd7f5dc56d7fda33aeffc9167a739176b9ed
amirch1 <amir.chervinsky@kaltura.com>
2023-07-20 08:19:53 +0300

    hackathon

--- src/app/modules/welcome/views/search-terms/search-terms.component.ts
+++ src/app/modules/welcome/views/search-terms/search-terms.component.ts
@@ -142,7 +142,7 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
       });
     const openai: OpenAIApi = new OpenAIApi(configuration);
 
-    let prompt = `below is a JSON array of search terms represented by the 'word' field and the number of times people searched for them represented by the 'count' field. Categorize the search terms into up to 5 categories and return only a JSON array of category items without line breaks. For each category set the field 'category' with the category name and the field 'count' with the total number of searches preformed on all the word items in this category.
+    let prompt = `below is a JSON array of search terms represented by the 'word' field and the number of times people searched for them represented by the 'count' field. Categorize the search terms into up to 5 categories and return only a JSON array of category items. For each category set the field 'category' with the category name and the field 'count' with the total number of searches preformed on all the word items in this category.
 
 [
   {"word": "workday", "count": 402},
@@ -191,7 +191,7 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
     if (this._showTrends && !this._trendsLoaded) {
       const categories = [];
       this._categories.forEach(category => categories.push(category.category));
-      prompt = `below is a JSON array of search terms represented by the 'word' field and the number of times people searched for them represented by the 'count' field. Categorize the search terms into these categories: ${categories.toString()} .return only a JSON array of category items without line breaks. For each category set the field 'category' with the category name and the field 'count' with the total number of searches performed on all the word items in this category.
+      prompt = `below is a JSON array of search terms represented by the 'word' field and the number of times people searched for them represented by the 'count' field. Categorize the search terms into these categories: ${categories.toString()} .return only a JSON array of category items. For each category set the field 'category' with the category name and the field 'count' with the total number of searches performed on all the word items in this category.
 
 [
   {"word": "recognition", "count": 679},
@@ -252,8 +252,9 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
       this._aiLoading = false;
     }).catch(error => {
       console.error(error?.response?.data?.error?.message || error);
-      this.showError = true;
       this._aiLoading = false;
+      this._trendsLoaded = false;
+      this.showError = true;
     });
   }
 
@@ -266,6 +267,8 @@ export class SearchTermsComponent implements OnInit, OnDestroy {
     if (this._showTrends) {
       this._trendsLoaded = true;
       let total = 0;
+      this.AIResponse = this.AIResponse.replace(/(\r\n|\n|\r)/gm, ""); // remove line breaks as sometimes they break parsing
+      this.AIResponse = this.AIResponse.replace(/'/g, '"'); // replace single quote with double quote
       const newCategoriesData = JSON.parse(this.AIResponse);
       const newCategories = [];
       if (newCategoriesData?.length) {
