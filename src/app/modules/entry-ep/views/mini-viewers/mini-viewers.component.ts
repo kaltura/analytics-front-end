1698bcc0c5cd5e4a170395af3a31b4e41e778514
amirch1 <amir.chervinsky@kaltura.com>
2023-07-24 13:43:26 +0300

    feat: add ep users export to Excel AN-22935

--- src/app/modules/entry-ep/views/mini-viewers/mini-viewers.component.ts
+++ src/app/modules/entry-ep/views/mini-viewers/mini-viewers.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Tab } from 'shared/components/report-tabs/report-tabs.component';
-import { KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaReportInterval, KalturaReportTotal, KalturaReportType } from 'kaltura-ngx-client';
-import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
-import { AuthService, BrowserService, ErrorsManagerService, ReportConfig, ReportService } from 'shared/services';
-import { switchMap } from 'rxjs/operators';
-import { of as ObservableOf } from 'rxjs';
-import { ReportDataConfig } from 'shared/services/storage-data-base.config';
-import { TranslateService } from '@ngx-translate/core';
-import { MiniViewersConfig } from './mini-viewers.config';
-import { KalturaLogger } from '@kaltura-ng/kaltura-logger';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Tab} from 'shared/components/report-tabs/report-tabs.component';
+import {KalturaClient, KalturaEndUserReportInputFilter, KalturaFilterPager, KalturaReportExportItem, KalturaReportExportParams, KalturaReportInterval,
+  KalturaReportResponseOptions, KalturaReportTotal, KalturaReportType, KalturaReportExportItemType, ReportExportToCsvAction} from 'kaltura-ngx-client';
+import {AreaBlockerMessage} from '@kaltura-ng/kaltura-ui';
+import {AppAnalytics, AuthService, BrowserService, ErrorsManagerService, ReportConfig, ReportService} from 'shared/services';
+import {finalize, switchMap} from 'rxjs/operators';
+import {of as ObservableOf} from 'rxjs';
+import {ReportDataConfig} from 'shared/services/storage-data-base.config';
+import {TranslateService} from '@ngx-translate/core';
+import {MiniViewersConfig} from './mini-viewers.config';
+import {KalturaLogger} from '@kaltura-ng/kaltura-logger';
 import {analyticsConfig} from "configuration/analytics-config";
-import * as moment from "moment";
 import {DateFilterUtils} from "shared/components/date-filter/date-filter-utils";
+import {cancelOnDestroy} from "@kaltura-ng/kaltura-common";
 
 @Component({
   selector: 'app-ep-mini-viewers',
@@ -23,7 +24,7 @@ import {DateFilterUtils} from "shared/components/date-filter/date-filter-utils";
     ReportService,
   ]
 })
-export class EpMiniViewersComponent implements OnInit {
+export class EpMiniViewersComponent implements OnInit, OnDestroy {
 
   private _dataConfig: ReportDataConfig;
   protected _componentId = 'ep-mini-viewers';
@@ -50,9 +51,11 @@ export class EpMiniViewersComponent implements OnInit {
   constructor(private _translate: TranslateService,
               private _reportService: ReportService,
               private _browserService: BrowserService,
+              private _analytics: AppAnalytics,
               private _errorsManager: ErrorsManagerService,
               private _authService: AuthService,
               private _dataConfigService: MiniViewersConfig,
+              private _kalturaClient: KalturaClient,
               private _logger: KalturaLogger) {
 
     this._dataConfig = _dataConfigService.getConfig();
@@ -102,7 +105,66 @@ export class EpMiniViewersComponent implements OnInit {
   }
 
   public exportLinks(): void {
-    console.log("export links");
+    this._isBusy = true;
+    this._blockerMessage = null;
+
+    const responseOptions = new KalturaReportResponseOptions({
+      delimiter: analyticsConfig.valueSeparator,
+      skipEmptyDates: analyticsConfig.skipEmptyBuckets
+    });
+
+    const reportsItemsGroup = 'Unique viewers';
+    const reportItems = [new KalturaReportExportItem({
+      action: KalturaReportExportItemType.table,
+      order: '-name',
+      reportTitle: 'Unique viewers',
+      reportType: KalturaReportType.epWebcastVodLiveUsersEngagement,
+      responseOptions,
+      filter: this._filter
+    })];
+
+    let baseUrl = '';
+    try {
+      const origin = window.parent && window.parent.location && window.parent.location.origin ? window.parent.location.origin : window.location.origin;
+      const isKMC = window.parent && typeof window.parent['kmcng'] === 'object';
+      const exportRoute = analyticsConfig.kalturaServer.exportRoute ? analyticsConfig.kalturaServer.exportRoute : isKMC ? '/index.php/kmcng/analytics/export?id=' : '/userreports/downloadreport?report_id=';
+      baseUrl = encodeURIComponent(`${origin}${exportRoute}`);
+    } catch (e) {
+      this._logger.error('Error accessing parent window location', e);
+    }
+
+    const exportAction = new ReportExportToCsvAction({ params: new KalturaReportExportParams(
+      { timeZoneOffset: this._filter.timeZoneOffset,
+        reportsItemsGroup,
+        reportItems,
+        baseUrl })
+    });
+
+    this._analytics.trackClickEvent('Export');
+
+    this._kalturaClient.request(exportAction)
+      .pipe(
+        cancelOnDestroy(this),
+        finalize(() => {
+          this._isBusy = false;
+        })
+      )
+      .subscribe(
+        () => {
+          this._browserService.alert({
+            header: this._translate.instant('app.exportReports.exportReports'),
+            message: this._translate.instant('app.exportReports.successMessage'),
+          });
+        },
+        () => {
+          this._browserService.alert({
+            header: this._translate.instant('app.exportReports.exportReports'),
+            message: this._translate.instant('app.exportReports.errorMessage'),
+          });
+        });
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
