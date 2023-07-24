b397418f454176be5d03230e5afaa208aa535132
amirch1 <amir.chervinsky@kaltura.com>
2023-07-24 14:09:48 +0300

    chore: bump analytics version to v3.2.1

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -103,7 +103,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '3.2.0',
+  appVersion: '3.2.1',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
