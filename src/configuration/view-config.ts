8616a72f00674aae8af01ba0a311973770194a74
amirch1 <amir.chervinsky@kaltura.com>
2024-09-24 08:30:36 +0300

    fix(category): Configuration to hide search users from category dashboard AN-23391

--- src/configuration/view-config.ts
+++ src/configuration/view-config.ts
@@ -176,6 +176,7 @@ export const viewsConfig = {
     performance: {
       userDrilldown: {},
       userFilter: {},
+      userSearch: {},
       userLink: {},
       entryDrilldown: {},
       entryFilter: {},
