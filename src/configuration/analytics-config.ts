5ed8aeca9d2bdf621940b19dd3df087a03387507
amirch1 <amir.chervinsky@kaltura.com>
2024-12-30 14:54:26 +0200

    chore: updated version

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -113,7 +113,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '3.7.9',
+  appVersion: '3.7.10',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
