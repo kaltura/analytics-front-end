18d46fa6d44d096c78696052ff5e6b57f42b2b37
amirch1 <amir.chervinsky@kaltura.com>
2025-01-21 09:04:03 +0200

    fix: allow email address change for report export

--- src/app/shared/components/export-csv/export-csv.component.ts
+++ src/app/shared/components/export-csv/export-csv.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnDestroy, Output, ViewChild} from '@angular/core';
 import {AppAnalytics, AuthService, BrowserService, ButtonType} from 'shared/services';
 import {DateChangeEvent} from 'shared/components/date-filter/date-filter.service';
-import {KalturaClient, KalturaEndUserReportInputFilter, KalturaObjectBaseFactory, KalturaPager, KalturaReportExportItem, KalturaReportExportParams, KalturaReportResponseOptions, ReportExportToCsvAction, UserGetAction, KalturaUser} from 'kaltura-ngx-client';
+import {KalturaClient, KalturaEndUserReportInputFilter, KalturaObjectBaseFactory, KalturaPager, KalturaReportExportItem, KalturaReportExportParams, KalturaReportResponseOptions, ReportExportToCsvAction} from 'kaltura-ngx-client';
 import {TreeNode} from 'primeng/api';
 import {TranslateService} from '@ngx-translate/core';
 import {PopupWidgetComponent} from '@kaltura-ng/kaltura-ui';
@@ -9,12 +9,11 @@ import {RefineFilter} from 'shared/components/filter/filter.component';
 import {refineFilterToServerValue} from 'shared/components/filter/filter-to-server-value.util';
 import {analyticsConfig} from 'configuration/analytics-config';
 import {DateFilterUtils} from 'shared/components/date-filter/date-filter-utils';
-import {cancelOnDestroy, tag} from '@kaltura-ng/kaltura-common';
+import {cancelOnDestroy} from '@kaltura-ng/kaltura-common';
 import {finalize} from 'rxjs/operators';
 import {ExportItem} from 'shared/components/export-csv/export-config-base.service';
 import {KalturaLogger} from "@kaltura-ng/kaltura-logger";
 import {isEmptyObject} from "shared/utils/is-empty-object";
-import {error} from "protractor";
 
 @Component({
   selector: 'app-export-csv',
@@ -236,19 +235,22 @@ export class ExportCsvComponent implements OnDestroy {
         this._logger.error('Error accessing parent window location', e);
       }
 
-      const exportAction = new ReportExportToCsvAction({ params: new KalturaReportExportParams({ timeZoneOffset, reportsItemsGroup, reportItems, baseUrl }) });
+      let params = new KalturaReportExportParams({ timeZoneOffset, reportsItemsGroup, reportItems, baseUrl });
+      if (analyticsConfig.customData?.exportEmail) {
+        params = new KalturaReportExportParams({ timeZoneOffset, reportsItemsGroup, reportItems, baseUrl, recipientEmail: analyticsConfig.customData.exportEmail});
+      }
+      const exportAction = new ReportExportToCsvAction({ params });
       const items = reportItems.map(item => item.reportTitle.toLowerCase().replace(' ', '_')).join(',');
       this._analytics.trackButtonClickEvent(ButtonType.Export, 'Export', items, this.feature);
       this.onExport.emit();
       this._exportingCsv = true;
+
       this._kalturaClient.request(exportAction)
         .pipe(
           cancelOnDestroy(this),
           finalize(() => {
             this._exportingCsv = false;
-            if (analyticsConfig.customData?.emailKs) {
-              this._kalturaClient.setDefaultRequestOptions({ks: this._authService.ks});
-            }
+
             if (this._popup) {
               this._popup.close();
             }
@@ -256,6 +258,10 @@ export class ExportCsvComponent implements OnDestroy {
         )
         .subscribe(
           () => {
+            // this._browserService.alert({
+            //   header: this._translate.instant('app.exportReports.exportReports'),
+            //   message: this._translate.instant('app.exportReports.successMessage'),
+            // });
           },
           () => {
             this._browserService.alert({
@@ -265,40 +271,17 @@ export class ExportCsvComponent implements OnDestroy {
           });
     };
 
-    const notifyUser = (emailAddress: string) => {
-      this._browserService.confirm(
-        {
-          header: this._translate.instant('app.exportReports.title'),
-          message: emailAddress ? this._translate.instant('app.exportReports.withEmail', {emailAddress}) : this._translate.instant('app.exportReports.withoutEmail'),
-          acceptLabel: this._translate.instant('app.exportReports.approve'),
-          rejectLabel: this._translate.instant('app.common.cancel'),
-          accept: () => {
-            doExport();
-          },
-          reject: () => {
-            if (analyticsConfig.customData?.emailKs) {
-              this._kalturaClient.setDefaultRequestOptions({ks: this._authService.ks});
-            }
-          }
-        });
-    };
-
-    if (analyticsConfig.customData?.emailKs) {
-      this._kalturaClient.setDefaultRequestOptions({ks: analyticsConfig.customData.emailKs});
-      this._kalturaClient.request(new UserGetAction())
-        .pipe(cancelOnDestroy(this))
-        .pipe(tag('block-shell'))
-        .subscribe(
-          (user: KalturaUser) => {
-            notifyUser(user.email);
-          },
-          error => {
-            notifyUser('');
-          }
-        );
-    } else {
-      notifyUser(this._authService.email || '');
-    }
+    const emailAddress = analyticsConfig.customData?.exportEmail ? analyticsConfig.customData.exportEmail : this._authService.email ? this._authService.email : null;
+    this._browserService.confirm(
+      {
+        header: this._translate.instant('app.exportReports.title'),
+        message: emailAddress ? this._translate.instant('app.exportReports.withEmail', {emailAddress}) : this._translate.instant('app.exportReports.withoutEmail'),
+        acceptLabel: this._translate.instant('app.exportReports.approve'),
+        rejectLabel: this._translate.instant('app.common.cancel'),
+        accept: () => {
+          doExport();
+        }
+      });
   }
   public toggleDropdown() {
     if (this._popup) {
