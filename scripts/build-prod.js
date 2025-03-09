b99a6f4891f669b4fb274d0491cb0d71766733db
amirch1 <amir.chervinsky@kaltura.com>
2025-03-09 17:13:38 +0200

    update build script

--- scripts/build-prod.js
+++ scripts/build-prod.js
@@ -53,7 +53,7 @@ console.log('analytics-config.ts has been updated!');
 const v2PlayerConfigFileName = "deploy/config.ini";
 const v2ConfigFilePath = path.resolve(packageRoot, v2PlayerConfigFileName);
 let v2ConfigData = fs.readFileSync(v2ConfigFilePath, 'utf8');
-const regex_v2 = /component.version=v\d\.\d\.\d/;
+const regex_v2 = /component.version=v\d\.\d\.\d\d/;
 v2ConfigData = v2ConfigData.replace(regex_v2, `component.version=v${versionNumber}`);
 
 fs.writeFileSync(v2ConfigFilePath, v2ConfigData, 'utf-8');
@@ -63,7 +63,7 @@ console.log('v2 player config has been updated!');
 const v7PlayerConfigFileName = "deploy_v7/config.ini";
 const v7ConfigFilePath = path.resolve(packageRoot, v7PlayerConfigFileName);
 let v7ConfigData = fs.readFileSync(v7ConfigFilePath, 'utf8');
-const regex_v7 = /component.version=v\d\.\d\.\d/;
+const regex_v7 = /component.version=v\d\.\d\.\d\d/;
 v7ConfigData = v7ConfigData.replace(regex_v7, `component.version=v${versionNumber}`);
 
 fs.writeFileSync(v7ConfigFilePath, v7ConfigData, 'utf-8');
