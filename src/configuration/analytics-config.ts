71beb5d03278bcb239e2423afa554afb53c80a33
amirch1 <amir.chervinsky@kaltura.com>
2025-04-20 10:32:05 +0300

    bump analytics version to v3.8.1

--- src/configuration/analytics-config.ts
+++ src/configuration/analytics-config.ts
@@ -116,7 +116,7 @@ export function buildCDNUrl(suffix: string): string {
 }
 
 export const analyticsConfig: AnalyticsConfig = {
-  appVersion: '3.8.0',
+  appVersion: '3.8.1',
   valueSeparator: '|',
   skipEmptyBuckets: false,
   defaultPageSize: 25,
