1cdae6b591bf875701058628f10a72a7801c8928
amirch1 <amir.chervinsky@kaltura.com>
2025-10-08 07:38:00 +0300

    fix: content on demand filter

--- src/app/modules/user-ep/views/content-on-demand/content-on-demand.component.ts
+++ src/app/modules/user-ep/views/content-on-demand/content-on-demand.component.ts
@@ -82,7 +82,7 @@ export class ContentOnDemandComponent implements OnDestroy {
     this._isBusy = true;
     this._blockerMessage = null;
     this._filter.virtualEventIdIn = this.eventIn;
-    //this._filter.userIds = this.userId;
+    this._filter.userIds = this.userId;
     this._filter.timeZoneOffset = DateFilterUtils.getTimeZoneOffset();
     this._filter.fromDate = Math.floor(this.startDate.getTime() / 1000);
     this._filter.toDate = Math.floor(new Date().getTime() / 1000);
