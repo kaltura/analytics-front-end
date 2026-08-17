72fd8fec54735fed44b869af13bae1f20906b75e
amirch1 <amir.chervinsky@kaltura.com>
2026-08-17 12:13:58 +0300

    cleanup

--- src/app/modules/entry-live/views/live-discovery-chart/live-discovery.component.ts
+++ src/app/modules/entry-live/views/live-discovery-chart/live-discovery.component.ts
@@ -37,7 +37,6 @@ export class LiveDiscoveryComponent implements OnInit, OnDestroy {
   public _isPolling: boolean;
   public _pollingBtnDisabled = false;
   public _rangeLabel: string;
-  public _streamFilter = 'none';
 
   constructor(private _liveExploreWidget: LiveDiscoveryWidget,
               private _liveDiscoveryTable: LiveDiscoveryTableWidget,
@@ -133,7 +132,6 @@ export class LiveDiscoveryComponent implements OnInit, OnDestroy {
   }
 
   public onStreamChange(stream: string): void {
-    this._streamFilter = stream;
     this._liveExploreWidget.updateStreamFilter(stream);
     this._liveDiscoveryTable.updateStreamFilter(stream);
   }
